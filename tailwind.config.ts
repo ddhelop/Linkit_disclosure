6fb423f6f182362ea5544fa39a5d200ad9b894eb
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-04-26 18:35:53 +0900

    fix: ModifyInitialSetting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
@@ -8,37 +8,36 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      tp: {
-        grey10: '#F6F8FC',
-        grey20: '#F1F4F9',
-        grey30: '#E2E8F0',
-        grey40: '#CBD4E1',
-        grey50: '#94A3B8',
-        grey60: '#64748B',
-        grey70: '#475569',
-        grey80: '#27364B',
-        grey90: '#1E2A3B',
-        grey100: '#0F1A2A',
-        grey01: '#FCFCFD',
-        blue10: '#D3E1FE',
-        blue20: '#7EA5F8',
-        blue40: '#4D82F3',
-        blue60: '#2563EB',
-        blue80: '#0037B3',
-        blue100: '#002987',
-        red10: '#FFD3DD',
-        red60: '#FF345F',
-      },
+      grey10: '#F6F8FC',
+      grey20: '#F1F4F9',
+      grey30: '#E2E8F0',
+      grey40: '#CBD4E1',
+      grey50: '#94A3B8',
+      grey60: '#64748B',
+      grey70: '#475569',
+      grey80: '#27364B',
+      grey90: '#1E2A3B',
+      grey100: '#0F1A2A',
+      grey01: '#FCFCFD',
+      blue10: '#D3E1FE',
+      blue20: '#7EA5F8',
+      blue40: '#4D82F3',
+      blue60: '#2563EB',
+      blue80: '#0037B3',
+      blue100: '#002987',
+      red10: '#FFD3DD',
+      red60: '#FF345F',
     },
-
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        'soft-shadow': '0px 1.7px 22.32px 0px rgba(0, 0, 0, 0.05)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
