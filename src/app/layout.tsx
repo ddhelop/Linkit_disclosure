8faad6419a94150257257a299de2ac9c34560ab2
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-05 01:57:52 +0900

    feat: fontUpdate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import Header from '@/components/Layout/Header'
 import Footer from '@/components/Layout/Footer'
 import './globals.css'
@@ -14,14 +15,21 @@ export const metadata: Metadata = {
   },
 }
 
+const pretendard = localFont({
+  src: '../lib/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>
         <Header />
         {children}
         <Footer />
