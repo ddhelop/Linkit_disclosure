7d8189a355d1e4d9aa22531a045f5d90986a302a
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-09 15:21:05 +0900

    fix: fixBug

--- src/app/onBoarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import OnBoardingPrivateInfo from '@/components/OnBoarding/OnBoardingPrivateInfo'
-
-export default function onBoardingPrivateInfoPage() {
-  return (
-    <div>
-      <OnBoardingPrivateInfo />
-    </div>
-  )
-}
