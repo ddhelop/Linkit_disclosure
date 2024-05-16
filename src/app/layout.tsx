8ebdc55d10a70dfa142112a5be160d7eead56f30
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-16 15:28:57 +0900

    fix: :bug: bugFix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,7 @@ export default function RootyLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const siteId = Number(process.env.NEXT_PUBLIC_HJID)
-  const hotjarVersion = Number(process.env.NEXT_PUBLIC_HJSV)
-
-  Hotjar.init(siteId, hotjarVersion)
+  Hotjar.init(4983615, 6)
 
   return (
     <html lang="ko" className={`${pretendard.variable}`}>
