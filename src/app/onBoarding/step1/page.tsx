4406e15c3be0924ebdaf3350910fc074fccb6196
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-17 18:20:35 +0900

    feat: :zap: onBoarding step1 애니메이션

--- src/app/onBoarding/step1/page.tsx
+++ src/app/onBoarding/step1/page.tsx
@@ -1,9 +1,12 @@
+import OnBoardingHeader from '@/components/Layout/onBoardingHeader'
 import OnBoardingStep1 from '@/components/OnBoarding/OnBoardingStep1'
 import React from 'react'
 
 export default function OnBoardingStep1Page() {
   return (
     <div>
+      <OnBoardingHeader />
+      <div className="w-1/3 h-[0.18rem] bg-[#2563EB]"></div>
       <OnBoardingStep1 />
     </div>
   )
