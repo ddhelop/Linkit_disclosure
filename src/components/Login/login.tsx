0dd5664cf6f7dea658a66aa7a3490d36afbea7ef
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-18 14:36:28 +0900

    fix: delete auth

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,19 +1,18 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { signIn, signOut, useSession } from 'next-auth/react'
 
 import Image from 'next/image'
 
 export default function Login() {
   return (
     <>
-      <div className="w-full h-screen flex">
+      <div className="flex h-screen w-full">
         {/* left container */}
-        <div className="hidden lg:flex lg:w-1/2 h-full bg-[#D3E1FE]"></div>
+        <div className="hidden h-full bg-[#D3E1FE] lg:flex lg:w-1/2"></div>
 
         {/* right container */}
-        <div className="lg:w-1/2 w-full h-full py-8 flex flex-col items-center justify-center">
+        <div className="flex h-full w-full flex-col items-center justify-center py-8 lg:w-1/2">
           <div className="flex flex-col items-center pt-44">
             <p className="text-lg font-medium">팀빌딩의 시작 링킷</p>
             <Image src={'/assets/icons/headerLogo.svg'} width={241} height={46} alt="linkit_login" className="pt-6" />
@@ -27,30 +26,24 @@ export default function Login() {
           </div>
 
           <div className="flex flex-col items-center gap-2 pt-6">
-            <div className="flex w-[23rem] h-[3.5rem] px-24 bg-[#00C73C] rounded items-center gap-1 cursor-pointer">
+            <div className="flex h-[3.5rem] w-[23rem] cursor-pointer items-center gap-1 rounded bg-[#00C73C] px-24">
               <Image src={'/assets/login/naverLogo.png'} width={39} height={44} alt="naverLogin" />
               <span className="font-semibold text-[#fff]">네이버로 시작하기</span>
             </div>
 
-            <div
-              className="flex w-[23rem] h-[3.5rem] px-24 bg-[#FFE500] rounded items-center gap-1 cursor-pointer"
-              onClick={() => signIn('kakao', { redirect: true, callbackUrl: '/onBoarding' })}
-            >
+            <div className="flex h-[3.5rem] w-[23rem] cursor-pointer items-center gap-1 rounded bg-[#FFE500] px-24">
               <Image src={'/assets/login/kakaoLogo.svg'} width={39} height={56} alt="kakaoLogin" />
               <span className="font-semibold">카카오로 시작하기</span>
             </div>
 
-            <div
-              className="flex w-[23rem] h-[3.5rem] px-[6.5rem] items-cente gap-3 items-center border-[1px] border-grey30 rounded cursor-pointer"
-              onClick={() => signOut()}
-            >
+            <div className="items-cente flex h-[3.5rem] w-[23rem] cursor-pointer items-center gap-3 rounded border-[1px] border-grey30 px-[6.5rem]">
               <Image src={'/assets/login/googleLogo.svg'} width={23} height={23} alt="NaverLogin" />
               <span className="font-semibold">구글로 시작하기</span>
             </div>
           </div>
 
-          <div className="w-full flex justify-center pt-28">
-            <span className="w-full text-xs text-grey60 font-normal text-center">
+          <div className="flex w-full justify-center pt-28">
+            <span className="w-full text-center text-xs font-normal text-grey60">
               회원가입시 linkit의 서비스 이용약관과 개인정보 보호정책에 동의하게됩니다.
             </span>
           </div>
