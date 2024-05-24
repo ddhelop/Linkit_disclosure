11adb6547d88a8230b8c32e4f9caa2895c5f578e
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-25 02:07:28 +0900

    refactor: Fix Foldeer Structure

--- src/app/onBoarding/step2/team/page.tsx
+++
@@ -1,9 +0,0 @@
-import OnBoardingStep2Team from '@/components/OnBoarding/team/OnBoardingStep2Team'
-
-export default function OnBoardingStep2TeamPage() {
-  return (
-    <div>
-      <OnBoardingStep2Team />
-    </div>
-  )
-}
