2bbbea5411348f7ccbcbe4d405f30f75571e6db8
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-18 17:07:25 +0900

    feat: 로그인 시 로딩 UI

--- src/components/Login/NaverRedirect.tsx
+++ src/components/Login/NaverRedirect.tsx
@@ -2,18 +2,20 @@
 import { emailState } from '@/context/recoil-context'
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
+import { motion } from 'framer-motion'
 
 // 카카오 로그인 후 서버에 전송할 코드를 받아오는 페이지
-export default function NaverRedirect() {
+const NaverRedirect: React.FC = () => {
   const params = useSearchParams()
   const code = params.get('code')
   const router = useRouter()
   const [toEmail, setToEmail] = useRecoilState(emailState)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const kakaoLogin = async () => {
+    const naverLogin = async () => {
       try {
         const response = await fetch(`https://dev.linkit.im/login/naver`, {
           method: 'POST',
@@ -36,14 +38,23 @@ export default function NaverRedirect() {
         }
       } catch (error) {
         console.log('로그인 요청에 실패했습니다.', error)
+      } finally {
+        setLoading(false)
       }
     }
-    kakaoLogin()
+    naverLogin()
   }, [code, router, setToEmail])
 
-  return (
-    <div>
-      <h1>네이버 로그인 후 서버에 전송할 코드를 받아오는 페이지</h1>
+  return loading ? (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <motion.div
+        className="border-t-blue-500 border-blue-200 h-12 w-12 animate-spin rounded-full border-4"
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1 }}
+      />
+      <p className="mt-4 text-lg">Loading...</p>
     </div>
-  )
+  ) : null
 }
+
+export default NaverRedirect
