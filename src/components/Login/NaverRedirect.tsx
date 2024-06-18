b29ba062a4aed9dff5510b8e5c11fd8ba21b2de5
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-18 16:40:16 +0900

    feat: Naver Login

--- src/components/Login/NaverRedirect.tsx
+++ src/components/Login/NaverRedirect.tsx
@@ -1,86 +1,49 @@
 'use client'
-import { setFullAuthData } from '@/features/auth/authSlice'
-import { useAppDispatch } from '@/hooks'
-import { AuthResponseData } from '@/lib/types'
+import { emailState } from '@/context/recoil-context'
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 
-// 네이버 로그인 후 서버에 전송할 코드를 받아오는 페이지
+// 카카오 로그인 후 서버에 전송할 코드를 받아오는 페이지
 export default function NaverRedirect() {
   const params = useSearchParams()
   const code = params.get('code')
   const router = useRouter()
-  const dispatch = useAppDispatch()
-
-  // silent refresh 요청 함수
-  const onSilentRefresh = useCallback(async () => {
-    try {
-      const response = await fetch('https://dev.linkit.im/token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json;charset=utf-8' },
-        credentials: 'include',
-      })
-
-      onLoginSuccess(await response.json())
-    } catch (error) {
-      console.log('silent refresh failed', error)
-    }
-  }, [dispatch])
-
-  // 로그인 성공 시 실행할 함수
-  const onLoginSuccess = useCallback(
-    (responseData: AuthResponseData) => {
-      dispatch(
-        setFullAuthData({
-          accessToken: responseData.accessToken,
-          email: responseData.email,
-          memberBasicInform: responseData.memberBasicInform,
-        }),
-      )
-      // 2시간-1분마다 silent refresh 요청
-      setTimeout(onSilentRefresh, 7200 * 1000 - 60000)
-    },
-    [dispatch, onSilentRefresh],
-  )
+  const [toEmail, setToEmail] = useRecoilState(emailState)
 
   useEffect(() => {
-    const naverLogin = async () => {
+    const kakaoLogin = async () => {
       try {
-        const response = await fetch('https://dev.linkit.im/login/naver', {
+        const response = await fetch(`https://dev.linkit.im/login/naver`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json;charset=utf-8' },
           credentials: 'include', // 쿠키를 포함시키기 위해 필요
           body: JSON.stringify({ code: code }), // 로그인 요청 본문에 인증 코드 포함
         })
-        if (response.status === 200 || response.status === 201) {
+        if (response.ok) {
           const responseData = await response.json()
-          onLoginSuccess(responseData)
-          router.push('/onBoarding')
+          window.localStorage.setItem('accessToken', responseData.accessToken)
+          setToEmail(responseData.email)
 
-          dispatch(
-            setFullAuthData({
-              accessToken: responseData.accessToken,
-              email: responseData.email,
-              memberBasicInform: responseData.memberBasicInform,
-            }),
-          )
-        } else {
-          const errorText = await response.text()
-          console.error('Response not ok, status:', response.status, 'text:', errorText)
-          throw new Error(`로그인 요청에 실패했습니다. 상태 코드: ${response.status}, 메시지: ${errorText}`)
+          if (responseData.existMemberBasicInform === true && responseData.existOnBoardingProfile === true) {
+            router.push('/')
+          } else if (responseData.existMemberBasicInform === true && responseData.existOnBoardingProfile === false) {
+            router.push(`/onBoarding/select`)
+          } else {
+            router.push(`/onBoarding`)
+          }
         }
       } catch (error) {
         console.log('로그인 요청에 실패했습니다.', error)
       }
     }
-
-    naverLogin()
-  }, [code, router, dispatch, onLoginSuccess])
+    kakaoLogin()
+  }, [code, router, setToEmail])
 
   return (
     <div>
-      <h1>카카오 로그인 후 서버에 전송할 코드를 받아오는 페이지</h1>
+      <h1>네이버 로그인 후 서버에 전송할 코드를 받아오는 페이지</h1>
     </div>
   )
 }
