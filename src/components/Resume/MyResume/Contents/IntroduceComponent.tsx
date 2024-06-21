fd49d4188c32aac9472f381afaf1e9283cc15dc1
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-22 01:23:35 +0900

    feat: 내 이력서 - 자기소개 데이터 연결

--- src/components/Resume/MyResume/Contents/IntroduceComponent.tsx
+++ src/components/Resume/MyResume/Contents/IntroduceComponent.tsx
@@ -1,10 +1,15 @@
 'use client'
+import { ProfileIntroductionResponse } from '@/lib/types'
 import { ChangeEvent, useState } from 'react'
 
-export default function IntroduceComponent() {
+interface MyResumeCompletionProps {
+  data: ProfileIntroductionResponse
+}
+
+export default function IntroduceComponent({ data }: MyResumeCompletionProps) {
   const [isEditing, setIsEditing] = useState(false)
-  const [introduction, setIntroduction] = useState('')
-  const [charCount, setCharCount] = useState(0)
+  const [introduction, setIntroduction] = useState(data.introduction || '')
+  const [charCount, setCharCount] = useState(data.introduction ? data.introduction.length : 0)
 
   const handleEditClick = () => {
     setIsEditing(true)
@@ -23,13 +28,13 @@ export default function IntroduceComponent() {
       {/* title */}
       <div className="flex items-center gap-[0.56rem]">
         <span className="text-lg font-semibold text-grey100">자기소개</span>
-        <span className="text-sm text-[#2563EB]">
+        {/* <span className="text-sm text-[#2563EB]">
           Tip : 본인의 경험을 바탕으로 핵심 역량과 보유 기술을 간단히 작성해주세요! (최대 300자)
-        </span>
+        </span> */}
       </div>
 
       {/* contents */}
-      <div className="pt-[0.94rem] ">
+      <div className="pt-[0.94rem]">
         {isEditing ? (
           <div>
             <textarea
@@ -42,7 +47,7 @@ export default function IntroduceComponent() {
             <div className="text-right text-sm text-grey100">{charCount} / 300자</div>
           </div>
         ) : (
-          <span>자기소개가 없습니다.</span>
+          <span className={introduction ? 'text-black' : 'text-grey50'}>{introduction || '자기소개가 없습니다.'}</span>
         )}
       </div>
 
