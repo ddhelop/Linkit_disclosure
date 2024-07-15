9f3ed32b2bb25dec89e34b65d7bed5832bef7209
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-16 02:06:54 +0900

    feat: 내 이력서 - 미니프로필 수정

--- src/components/Resume/MyResume/Nav/MyResumeNavProfile.tsx
+++ src/components/Resume/MyResume/Nav/MyResumeNavProfile.tsx
@@ -1,5 +1,11 @@
+'use client'
+import { useState, useEffect } from 'react'
 import { JobAndSkillResponse, MemberNameResponse, MiniProfileResponse } from '@/lib/types'
+import { GetOnBoardingData } from '@/lib/action'
 import Image from 'next/image'
+import MiniProfileModal from './MiniProfileModal'
+import { useRecoilState } from 'recoil'
+import { accessTokenState } from '@/context/recoil-context'
 
 interface MyResumeNavProfileProps {
   data: MiniProfileResponse
@@ -8,15 +14,46 @@ interface MyResumeNavProfileProps {
 }
 
 export default function MyResumeNavProfile({ data, name, jobAndSkill }: MyResumeNavProfileProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [profileData, setProfileData] = useState<MiniProfileResponse | null>(data)
+  const [jobAndSkillData, setJobAndSkillData] = useState<JobAndSkillResponse | null>(jobAndSkill)
+  const accessToken = useRecoilState(accessTokenState)[0] || ''
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+    fetchProfileData() // 모달이 닫힐 때 데이터 리패칭
+  }
+
+  const fetchProfileData = async () => {
+    if (!accessToken) return
+    try {
+      const response = await GetOnBoardingData(accessToken)
+      if (response.miniProfileResponse && response.jobAndSkillResponse) {
+        setProfileData(response.miniProfileResponse)
+        setJobAndSkillData(response.jobAndSkillResponse)
+      }
+    } catch (error) {
+      console.error('Failed to fetch updated profile data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProfileData()
+  }, [accessToken])
+
   return (
     <div className="flex w-full flex-col rounded-2xl border border-grey30 bg-[#fff] px-[1.02rem] py-[0.77rem]">
       {/* title */}
       <div className="flex w-full justify-between pt-[0.43rem]">
-        <h3 className="text-grey-100 w-[60%] text-[1.279rem] font-bold">{data?.profileTitle}</h3>
+        <h3 className="text-grey-100 w-[60%] text-[1.279rem] font-bold">{profileData?.profileTitle}</h3>
       </div>
 
       <div className="mt-[0.77rem] flex gap-[0.26rem]">
-        {data.myKeywordNames?.map((keyword, index) => (
+        {profileData?.myKeywordNames?.map((keyword, index) => (
           <span
             key={index}
             className="mb-[2rem] rounded-[0.45rem] bg-grey10 px-[0.57rem] py-[0.18rem] text-[0.89rem] text-grey60"
@@ -29,7 +66,7 @@ export default function MyResumeNavProfile({ data, name, jobAndSkill }: MyResume
       {/* profile */}
       <div className="flex w-full items-center gap-4 ">
         <Image
-          src={data?.miniProfileImg ? data?.miniProfileImg : '/assets/images/DefaultProfile.png'}
+          src={profileData?.miniProfileImg ? profileData.miniProfileImg : '/assets/images/DefaultProfile.png'}
           width={42}
           height={42}
           alt="profile"
@@ -37,17 +74,22 @@ export default function MyResumeNavProfile({ data, name, jobAndSkill }: MyResume
         />
 
         <div className="flex flex-col justify-center">
-          <span className=" text-[0.76rem] font-semibold text-grey90">{data?.memberName}</span>
+          <span className=" text-[0.76rem] font-semibold text-grey90">{profileData?.memberName}</span>
           <div className="flex gap-2">
-            {jobAndSkill.jobRoleNames?.map((job, index) => (
+            {jobAndSkillData?.jobRoleNames?.map((job, index) => (
               <span key={index} className="text-xs text-grey60">
                 {job}
-                {index < jobAndSkill.jobRoleNames.length - 1 && ', '}
+                {index < jobAndSkillData.jobRoleNames.length - 1 && ', '}
               </span>
             ))}
           </div>
         </div>
       </div>
+      <button onClick={handleModalOpen} className="mt-[0.88rem] rounded-[0.25rem] bg-grey20 py-[0.56rem] text-sm">
+        수정하기
+      </button>
+
+      <MiniProfileModal isOpen={isModalOpen} onClose={handleModalClose} />
     </div>
   )
 }
