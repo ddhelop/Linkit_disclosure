29a91f7590adb8bb05146779996ba85cd69af5f1
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-22 01:48:06 +0900

    feat: 매칭 요청 수락/거절 팝업 모달

--- src/components/Match/AccomplishMatch.tsx
+++ src/components/Match/AccomplishMatch.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Image from 'next/image'
+
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { GetMatchAccomplished } from '@/lib/action'
@@ -21,17 +21,21 @@ export default function AccomplishMatch() {
         console.error(error)
       }
     }
-    getMatchReceived()
-  }, [accessToken])
+
+    // 데이터가 중복으로 불러오는 것을 방지하기 위해 빈 배열 체크 후 데이터 불러오기
+    if (matchReceived.length === 0) {
+      getMatchReceived()
+    }
+  }, [accessToken, matchReceived.length])
 
   return (
     <div className="flex w-full flex-col pt-12">
       <div className="flex flex-col gap-[0.31rem]">
-        <h1 className="text-2xl font-bold">내가 받은 매칭</h1>
+        <h1 className="text-2xl font-bold">성사된 매칭</h1>
         <p className="text-grey60">공모전부터 사이드 프로젝트, 창업 초기 멤버까지 함께 할 팀원을 찾아 보세요!</p>
       </div>
 
-      <div className="mt-[2.65rem] flex flex-col">
+      <div className="mt-[2.65rem] flex flex-col gap-2">
         {matchReceived.length === 0 && (
           // 매칭 알림이 없어요 메세지
           <div className="flex items-center gap-[0.5rem]">
@@ -54,7 +58,10 @@ export default function AccomplishMatch() {
                 <p className="font-semibold">{match.successMatchingMemberName}님과 매칭이 성사되었습니다</p>
                 <p className="text-sm text-grey60">{match.requestMessage}</p>
               </div>
-              <p className="text-xs text-grey50">{match.requestOccurTime}</p>
+              <div className="flex flex-col justify-between">
+                <p className="text-xs text-grey50">{match.requestOccurTime}</p>
+                <button className="rounded-[0.3rem] bg-main px-6 py-2 text-sm text-white">연락하기</button>
+              </div>
             </div>
           </motion.div>
         ))}
--- src/components/Match/common/ResponseMatchModat.tsx
+++ src/components/Match/common/ResponseMatchModat.tsx
@@ -1,5 +1,6 @@
+// ResponseMatchModal.tsx
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { MatchReceivedType } from '@/lib/types'
@@ -14,6 +15,7 @@ interface ModalProps {
 
 export default function ResponseMatchModat({ match, onClose }: ModalProps) {
   const accessToken = useRecoilValue(accessTokenState) || ''
+  const [confirmModal, setConfirmModal] = useState<null | boolean>(null)
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -45,48 +47,86 @@ export default function ResponseMatchModat({ match, onClose }: ModalProps) {
       } else if (match.matchingType === 'TEAM') {
         await PostTeamMatchResponse(accessToken, match.receivedMatchingId, isAllowMatching)
       }
+      setConfirmModal(null)
       onClose()
     } catch (error) {
       console.error('Failed to send match response:', error)
     }
   }
 
-  return (
+  const renderConfirmModal = () => (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <motion.div
-        className="modal-content w-[48.5rem] rounded-lg bg-white p-6 shadow-lg"
+        className="modal-content w-[20rem] rounded-lg bg-white p-6 shadow-lg"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 50 }}
       >
         <div className="flex justify-center">
-          <h2 className="font-semibold text-grey100">{match.senderName}께 매칭 요청을 받았습니다</h2>
+          <Image src="/assets/icons/gray-warning.svg" width={30} height={30} alt="alert" className="rounded-full" />
         </div>
-        <div className="mt-4 flex items-center">
-          <Image
-            src={match.miniProfileImg || '/assets/images/DefaultProfile.png'}
-            width={65}
-            height={65}
-            alt="Sender Profile"
-            className="rounded-full"
-          />
-          <div className="ml-4">
-            <h3 className="font-semibold">{match.senderName}</h3>
-            <p className="text-sm text-grey60">{match.jobRoleNames}</p>
-          </div>
+        <div className="flex justify-center pt-3">
+          <h2 className="font-semibold text-grey100">{confirmModal ? '정말 수락할까요?' : '정말 거절할까요?'}</h2>
         </div>
-        <div className="mt-4 rounded bg-grey20 p-5 text-sm">
-          <p>{match.requestMessage}</p>
-        </div>
-        <div className="mt-7 flex justify-end">
-          <button className="mr-2 rounded bg-grey20 px-10 py-2 text-grey60" onClick={() => handleResponse(false)}>
-            거절
+        <div className="mt-7 flex justify-center">
+          <button
+            className="mr-2 rounded-[0.6rem] bg-grey20 px-10 py-2 text-grey60"
+            onClick={() => setConfirmModal(null)}
+          >
+            취소
           </button>
-          <button className="rounded bg-main px-10 py-2 text-white" onClick={() => handleResponse(true)}>
-            수락
+          <button
+            className="rounded-[0.6rem] bg-grey90 px-11 py-2 text-white"
+            onClick={() => handleResponse(confirmModal as boolean)}
+          >
+            네
           </button>
         </div>
       </motion.div>
     </div>
+  )
+
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        {!confirmModal && (
+          <motion.div
+            className="modal-content w-[48.5rem] rounded-lg bg-white p-6 shadow-lg"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+          >
+            <div className="flex justify-center">
+              <h2 className="font-semibold text-grey100">{match.senderName}께 매칭 요청을 받았습니다</h2>
+            </div>
+            <div className="mt-4 flex items-center">
+              <Image
+                src={match.miniProfileImg || '/assets/images/DefaultProfile.png'}
+                width={65}
+                height={65}
+                alt="Sender Profile"
+                className="rounded-full"
+              />
+              <div className="ml-4">
+                <h3 className="font-semibold">{match.senderName}</h3>
+                <p className="text-sm text-grey60">{match.jobRoleNames}</p>
+              </div>
+            </div>
+            <div className="mt-4 rounded bg-grey20 p-5 text-sm">
+              <p>{match.requestMessage}</p>
+            </div>
+            <div className="mt-7 flex justify-end">
+              <button className="mr-2 rounded bg-grey20 px-10 py-2 text-grey60" onClick={() => setConfirmModal(false)}>
+                거절
+              </button>
+              <button className="rounded bg-main px-10 py-2 text-white" onClick={() => setConfirmModal(true)}>
+                수락
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </div>
+      {confirmModal !== null && renderConfirmModal()}
+    </>
   )
 }
