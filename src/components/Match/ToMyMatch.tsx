6d7893407bcef2445f3b912cdf62ff9d3f543b96
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-22 02:47:46 +0900

    fix: key 에러 수정

--- src/components/Match/ToMyMatch.tsx
+++ src/components/Match/ToMyMatch.tsx
@@ -39,9 +39,9 @@ export default function FromMyMatch() {
           </div>
         )}
 
-        {matchReceived.map((match) => (
+        {matchReceived.map((match, index) => (
           <motion.div
-            key={match.id}
+            key={`${match.id}-${index}`}
             className="flex w-[48.5rem] cursor-pointer  gap-[1.44rem] rounded-lg bg-[#fff] p-5 shadow-sm"
             whileHover={{
               y: -3,
