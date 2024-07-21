f8d408f76679dfd6e9b4c6f163c2aa384b0b84eb
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-22 02:59:07 +0900

    style: 네비게이션 높이 수정

--- src/app/match/save/page.tsx
+++ src/app/match/save/page.tsx
@@ -4,12 +4,12 @@ import SaveProfile from '@/components/Match/SaveProfile'
 
 export default function SaveProfilePage() {
   return (
-    <div className="flex w-full gap-9 pt-[61px]">
+    <div className="flex w-full justify-center gap-9 pt-[61px]">
       {/* 네비게이션 바 */}
-      <div className="flex w-[40%] justify-end">
+      <div className="flex w-[20%] justify-end">
         <MatchNavigation />
       </div>
-      <div className="w-full">
+      <div className="w-[80rem]">
         <div className="flex flex-col gap-[0.31rem] pt-12">
           <h1 className="text-2xl font-bold">찜한 내역</h1>
         </div>
--- src/components/Match/MatchNavigation.tsx
+++ src/components/Match/MatchNavigation.tsx
@@ -6,43 +6,45 @@ export default function MatchNavigation() {
   const pathname = usePathname()
 
   return (
-    <div className="mt-[9.5rem] flex w-[13.75rem] flex-col rounded-2xl bg-[#fff] shadow-sm">
-      <Link href="/match/from">
-        <div
-          className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
-            pathname === '/match/from' ? 'font-bold text-grey100' : ''
-          }`}
-        >
-          내가 받은 매칭
-        </div>
-      </Link>
-      <Link href="/match/to">
-        <div
-          className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
-            pathname === '/match/to' ? 'font-bold text-grey100' : ''
-          }`}
-        >
-          내가 보낸 매칭
-        </div>
-      </Link>
-      <Link href="/match/accomplish">
-        <div
-          className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
-            pathname === '/match/accomplish' ? 'font-bold text-grey100' : ''
-          }`}
-        >
-          성사된 매칭
-        </div>
-      </Link>
-      <Link href="/match/save">
-        <div
-          className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
-            pathname === ('/match/save' || '/match/save/team') ? 'font-bold text-grey100' : ''
-          }`}
-        >
-          찜한 내역
-        </div>
-      </Link>
+    <div>
+      <div className="mt-[9.5rem] flex h-auto w-[13.75rem] flex-col rounded-2xl bg-[#fff] shadow-sm">
+        <Link href="/match/from">
+          <div
+            className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
+              pathname === '/match/from' ? 'font-bold text-grey100' : ''
+            }`}
+          >
+            내가 받은 매칭
+          </div>
+        </Link>
+        <Link href="/match/to">
+          <div
+            className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
+              pathname === '/match/to' ? 'font-bold text-grey100' : ''
+            }`}
+          >
+            내가 보낸 매칭
+          </div>
+        </Link>
+        <Link href="/match/accomplish">
+          <div
+            className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
+              pathname === '/match/accomplish' ? 'font-bold text-grey100' : ''
+            }`}
+          >
+            성사된 매칭
+          </div>
+        </Link>
+        <Link href="/match/save">
+          <div
+            className={`cursor-pointer px-5 py-[0.81rem] text-sm text-grey60 hover:bg-grey10 ${
+              pathname === ('/match/save' || '/match/save/team') ? 'font-bold text-grey100' : ''
+            }`}
+          >
+            찜한 내역
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
