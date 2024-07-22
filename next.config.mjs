fd713107fa234b785d34896ca3d5e731e3ebd604
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-23 02:13:47 +0900

    fix: 이미지 도메인 허용

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false, // 개발 모드에서 Strict Mode를 비활성화
   images: {
-    domains: ['image.linkit.im'], // 여기에 외부 이미지 도메인을 추가합니다.
+    domains: ['image.linkit.im', 'image-prod.linkit.im'], // 여기에 외부 이미지 도메인을 추가합니다.
   },
 }
 
