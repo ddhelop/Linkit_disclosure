940150e6b20949a192f2eb969be39803a29bbd39
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-23 04:27:38 +0900

    feat: 404 +

--- src/app/[...404]/page.tsx
+++ src/app/[...404]/page.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="h-screen w-full flex-col items-center justify-center bg-white">
-      <div>
+      <div className="flex h-screen w-full flex-col items-center justify-center">
         <Image src="/assets/images/404_image.svg" alt="404" width={455} height={232} />
         <Link href="/">
-          <button className="rounded-lg border border-main px-[1.44rem] py-3 text-main">링킷 사이트 바로가기</button>
+          <button className="mt-[2.3rem] rounded-lg border border-main px-[1.44rem] py-3 text-main">
+            링킷 사이트 바로가기
+          </button>
         </Link>
       </div>
     </div>
