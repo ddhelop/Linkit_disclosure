46c7a82c55a0eeb5154b62ae8de148290d51e372
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-27 02:50:10 +0900

    style: 404 페이지 스타일 변경

--- src/app/[...404]/page.tsx
+++ src/app/[...404]/page.tsx
@@ -5,9 +5,14 @@ export default function NotFound() {
   return (
     <div className="h-screen w-full flex-col items-center justify-center bg-white">
       <div className="flex h-screen w-full flex-col items-center justify-center">
-        <Image src="/assets/images/404_image.svg" alt="404" width={455} height={232} />
+        <Image src="/assets/images/404_image.svg" alt="404" width={330} height={212} />
+        <div className="pt-[0.61rem] text-center text-main">
+          원하시는 결과를 찾을 수 없습니다.
+          <br />
+          올바른 URL이 입력되었는지 확인해주세요
+        </div>
         <Link href="/">
-          <button className="mt-[2.3rem] rounded-lg border border-main px-[1.44rem] py-3 text-main">
+          <button className="mt-[1.19rem] rounded-lg border border-main px-[1.44rem] py-3 text-main">
             링킷 사이트 바로가기
           </button>
         </Link>
