7a70c8d2a66f3cae1d0954d37314e2abbc9cc1fb
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-27 15:46:06 +0900

    fix: 팀원 찾기 페이지 버그 수정

--- src/app/findMember/page.tsx
+++ src/app/findMember/page.tsx
@@ -1,5 +1,7 @@
 // FindingMemberPage.tsx
 import Banner from '@/components/Find/Banner'
+import FindMember from '@/components/Find/Member/FindMember'
+import FindMemberLeftNav from '@/components/Find/Member/FindMemberLeftNav'
 import FindTeam from '@/components/Find/Team/FindTeam'
 import FindTeamLeftNav from '@/components/Find/Team/FindTeamLeftNav'
 
@@ -10,17 +12,17 @@ export default function FindingMemberPage() {
         imgSrc="/assets/images/Find_Private_Banner.png"
         title="🚀 팀원 찾기"
         description="공모전부터 사이드 프로젝트, 창업 팀까지 합류하고 싶은 팀을 찾아보세요!"
-        scrolledTitle="👋 팀 찾기"
+        scrolledTitle="👋 팀원 찾기"
         scrolledDescription="공모전부터 사이드 프로젝트, 창업 초기 멤버까지 함께 할 팀원을 찾아 보세요!"
       />
 
       <div className="flex w-full justify-center bg-grey10 pb-24 pt-[18rem]">
         <div className="flex w-full justify-center gap-[1.5rem]">
           <div className="flex w-[16rem] justify-end">
-            <FindTeamLeftNav />
+            <FindMemberLeftNav />
           </div>
           <div className="w-[47rem]">
-            <FindTeam />
+            <FindMember />
           </div>
         </div>
       </div>
