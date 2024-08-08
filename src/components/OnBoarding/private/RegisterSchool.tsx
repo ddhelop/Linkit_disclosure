dd1cc19bfd13d97785793d771e48d127174e116e
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-08 20:11:30 +0900

    feat: 개인 온보딩 - 학력 모바일 반응형

--- src/components/OnBoarding/private/RegisterSchool.tsx
+++ src/components/OnBoarding/private/RegisterSchool.tsx
@@ -1,36 +1,23 @@
+// RegisterSchool.tsx
 'use client'
-import Image from 'next/image'
-import { useForm, SubmitHandler } from 'react-hook-form'
 import React, { useEffect, useState } from 'react'
-import { DeleteSchoolData, GetOnBoardingData, PostOneSchoolData } from '@/lib/action'
 import { useRouter } from 'next/navigation'
 import { useRecoilValue } from 'recoil'
-import { accessTokenState } from '@/context/recoil-context'
 import Link from 'next/link'
+import { DeleteSchoolData, GetOnBoardingData, PostOneSchoolData } from '@/lib/action'
+import { accessTokenState } from '@/context/recoil-context'
 import OnBoardingHeader from '../OnBoardingHeader'
-
-export interface SchoolFormInputs {
-  id: number
-  universityName: string
-  majorName: string
-  admissionYear: number
-  graduationYear: number | null
-  degreeName: string
-}
+import { EducationForm, SchoolFormInputs } from '@/components/common/component/onBoarding/EducationForm'
+import { EducationItem } from '@/components/common/component/onBoarding/EducationItem'
+import { SubmitHandler } from 'react-hook-form'
+import { pushNotification } from '@/components/common/component/ToastPopUp/ToastPopup'
 
 export default function RegisterSchool() {
   const [educationList, setEducationList] = useState<SchoolFormInputs[]>([])
-  const { register, handleSubmit, reset, setValue, watch } = useForm<SchoolFormInputs>({
-    defaultValues: {
-      degreeName: '졸업', // 기본 값을 '졸업'으로 설정
-    },
-  })
   const [editingIndex, setEditingIndex] = useState<number | null>(null)
   const [isClient, setIsClient] = useState(false)
   const [currentIndex, setCurrentIndex] = useState<number | null>(null)
-  const router = useRouter()
   const accessToken = useRecoilValue(accessTokenState) || ''
-  const degreeName = watch('degreeName')
 
   useEffect(() => {
     setIsClient(true)
@@ -57,9 +44,9 @@ export default function RegisterSchool() {
     }
   }, [accessToken])
 
-  const onSubmit: SubmitHandler<SchoolFormInputs> = async (data) => {
+  const handleFormSubmit: SubmitHandler<SchoolFormInputs> = async (data) => {
     const educationData = {
-      admissionYear: Number(data.admissionYear), // 숫자로 변환
+      admissionYear: Number(data.admissionYear),
       graduationYear: Number(data.graduationYear),
       universityName: data.universityName,
       majorName: data.majorName,
@@ -68,7 +55,9 @@ export default function RegisterSchool() {
 
     const response = await PostOneSchoolData(accessToken, educationData)
     if (response.ok) {
+      pushNotification('학력 정보가 성공적으로 등록되었습니다.', 'success')
     } else {
+      pushNotification('학력 정보 등록에 실패했습니다.', 'error')
     }
 
     const updatedData = {
@@ -84,31 +73,30 @@ export default function RegisterSchool() {
     } else {
       setEducationList((prev) => [...prev, updatedData])
     }
-    reset({ degreeName: '졸업' }) // Reset 할 때 기본 값을 '졸업'으로 설정
   }
 
-  const handleEdit = async (index: number) => {
-    const education = educationList[index]
-    setValue('universityName', education.universityName)
-    setValue('majorName', education.majorName)
-    setValue('admissionYear', education.admissionYear)
-    setValue('graduationYear', education.graduationYear)
-    setValue('degreeName', education.degreeName)
+  const handleEdit = (index: number) => {
     setEditingIndex(index)
     setCurrentIndex(index)
   }
 
   const handleDelete = async (index: number) => {
     if (window.confirm('정말로 삭제하시겠습니까?')) {
-      const response = await DeleteSchoolData(accessToken, educationList[index].id)
+      const response = await DeleteSchoolData(accessToken, educationList[index].id!)
       if (response.ok) {
         setEducationList((prev) => prev.filter((_, i) => i !== index))
         if (index === editingIndex) {
           setEditingIndex(null)
-          reset({ degreeName: '졸업' }) // Reset 할 때 기본 값을 '졸업'으로 설정
         }
+        pushNotification('학력 정보가 성공적으로 삭제되었습니다.', 'success')
       }
     }
+  }
+
+  const handleCancel = () => {
+    // Logic to close the form when cancel is clicked
+    setEditingIndex(null)
+    setCurrentIndex(null)
   }
 
   if (!isClient) {
@@ -116,184 +104,48 @@ export default function RegisterSchool() {
   }
 
   return (
-    <div className="flex h-screen flex-col bg-[#fff] lg:py-[69px]">
+    <div className="flex h-screen flex-col bg-[#fff] px-4 pt-6 lg:py-[69px]">
       <OnBoardingHeader percentage={55} />
       <div className="flex flex-grow flex-col items-center py-16">
-        <div className="flex w-[90%] justify-between text-sm font-medium leading-9 text-grey60 sm:w-[55%]">
+        <div className="flex w-full justify-between text-sm font-medium leading-9 text-grey60 md:w-[55%]">
           <span>내 이력서 가이드</span>
         </div>
-        <div className="flex w-[90%] flex-col items-start leading-9 sm:w-[55%]">
+        <div className="flex w-full flex-col items-start leading-9 md:w-[55%]">
           <span className="text-2xl font-bold">나의 학력을 등록해 주세요</span>
         </div>
 
         {educationList.map((education, index) => (
-          <div
-            key={index}
-            className="mt-6 flex w-[90%] flex-col rounded-[0.63rem] border border-grey30 px-5 py-6 lg:w-[55%]"
-          >
-            <div className="flex justify-between">
-              <div className="flex flex-col">
-                <span className="font-semibold">{education.universityName}</span>
-                <span className="pt-2 text-sm text-grey60">{education.majorName}</span>
-                <span className="text-xs text-grey50">
-                  {education.admissionYear} - {education.graduationYear || ''} ({education.degreeName})
-                </span>
-              </div>
-              <div className="flex items-center justify-end">
-                <Image
-                  src="/assets/icons/pencil.svg"
-                  width={27}
-                  height={27}
-                  alt="edit"
-                  className="cursor-pointer"
-                  onClick={() => handleEdit(index)}
-                />
-                <Image
-                  src="/assets/icons/delete.svg"
-                  width={27}
-                  height={27}
-                  alt="delete"
-                  className="cursor-pointer"
-                  onClick={() => handleDelete(index)}
-                />
-              </div>
-            </div>
+          <div key={index} className="flex w-full flex-col items-center">
+            <EducationItem
+              education={education}
+              onEdit={() => handleEdit(index)}
+              onDelete={() => handleDelete(index)}
+              isEditing={editingIndex === index}
+            />
             {editingIndex === index && (
-              <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="mt-6 flex flex-col rounded-[0.63rem] border border-grey30 px-5 py-6"
-              >
-                <div className="flex gap-3">
-                  <div className="flex w-[49%] flex-col">
-                    <span className="text-sm font-normal text-grey100">
-                      학교명<span className="pl-1 text-[#2563EB]">*</span>
-                    </span>
-                    <input
-                      type="text"
-                      placeholder="대학교(원)"
-                      className="mt-2 rounded-[0.31rem] border border-grey40 px-[0.88rem] py-2 text-sm"
-                      {...register('universityName', { required: true })}
-                    />
-                  </div>
-
-                  <div className="flex w-[49%] flex-col">
-                    <span className="text-sm font-normal text-grey100">
-                      전공명<span className="pl-1 text-[#2563EB]">*</span>
-                    </span>
-                    <input
-                      type="text"
-                      placeholder="경영학과"
-                      className="mt-2 rounded-[0.31rem] border border-grey40 px-[0.88rem] py-2 text-sm"
-                      {...register('majorName', { required: true })}
-                    />
-                  </div>
-                </div>
-
-                <div className="mt-6 flex flex-col">
-                  <span className="text-sm font-normal text-grey100">
-                    재학 기간<span className="pl-1 text-[#2563EB]">*</span>
-                  </span>
-                  <div className="flex justify-between">
-                    <div className="mt-2 flex gap-4">
-                      <input
-                        className="h-10 w-20 rounded-[0.31rem] border border-grey40 px-5 text-sm"
-                        placeholder="입학연도"
-                        {...register('admissionYear', { required: true })}
-                      />
-                      <Image src="/assets/icons/~.svg" width={8} height={29} alt="~" />
-                      <input
-                        className="h-10 w-20 rounded-[0.31rem] border border-grey40 px-5 text-sm"
-                        placeholder="졸업연도"
-                        {...register('graduationYear', { required: degreeName !== '재학' })}
-                        disabled={degreeName === '재학'}
-                      />
-                      <select
-                        className="w-20 rounded-md border border-grey40 text-center text-sm text-grey80"
-                        {...register('degreeName', { required: true })}
-                      >
-                        <option value="재학">재학</option>
-                        <option value="졸업">졸업</option>
-                      </select>
-                    </div>
-                    <button
-                      type="submit"
-                      className="cursor-pointer rounded-md bg-[#2563EB] px-[0.88rem] text-sm text-[#fff]"
-                    >
-                      수정하기
-                    </button>
-                  </div>
-                </div>
-              </form>
+              <EducationForm
+                defaultValues={education}
+                onSubmit={handleFormSubmit}
+                onCancel={handleCancel}
+                isEditMode={true}
+              />
             )}
           </div>
         ))}
 
         {editingIndex === null && (
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="mt-6 flex w-[90%] flex-col rounded-[0.63rem] border border-grey30 px-5 py-6 lg:w-[55%]"
-          >
-            <div className="flex gap-3">
-              <div className="flex w-[49%] flex-col">
-                <span className="text-sm font-normal text-grey100">
-                  학교명<span className="pl-1 text-[#2563EB]">*</span>
-                </span>
-                <input
-                  type="text"
-                  placeholder="대학교(원)"
-                  className="mt-2 rounded-[0.31rem] border border-grey40 px-[0.88rem] py-2 text-sm"
-                  {...register('universityName', { required: true })}
-                />
-              </div>
-
-              <div className="flex w-[49%] flex-col">
-                <span className="text-sm font-normal text-grey100">
-                  전공명<span className="pl-1 text-[#2563EB]">*</span>
-                </span>
-                <input
-                  type="text"
-                  placeholder="경영학과"
-                  className="mt-2 rounded-[0.31rem] border border-grey40 px-[0.88rem] py-2 text-sm"
-                  {...register('majorName', { required: true })}
-                />
-              </div>
-            </div>
-
-            <div className="mt-6 flex flex-col">
-              <span className="text-sm font-normal text-grey100">
-                재학 기간<span className="pl-1 text-[#2563EB]">*</span>
-              </span>
-              <div className="flex flex-col justify-between gap-5 lg:flex-row">
-                <div className="mt-2 flex gap-4">
-                  <input
-                    className="h-10 w-20 rounded-[0.31rem] border border-grey40 text-center text-sm"
-                    placeholder="입학연도"
-                    {...register('admissionYear', { required: true })}
-                  />
-                  <Image src="/assets/icons/~.svg" width={8} height={29} alt="~" />
-                  <input
-                    className="h-10 w-20 rounded-[0.31rem] border border-grey40 text-center text-sm"
-                    placeholder="졸업연도"
-                    {...register('graduationYear', { required: degreeName !== '재학' })}
-                    disabled={degreeName === '재학'}
-                  />
-                  <select
-                    className="w-20 rounded-md border border-grey40 text-center text-sm text-grey80"
-                    {...register('degreeName', { required: true })}
-                  >
-                    <option value="재학">재학</option>
-                    <option value="졸업">졸업</option>
-                  </select>
-                </div>
-                <button
-                  type="submit"
-                  className="cursor-pointer rounded-md bg-[#2563EB] px-[0.88rem] py-2 text-sm text-[#fff] lg:py-0"
-                >
-                  추가하기
-                </button>
-              </div>
-            </div>
-          </form>
+          <EducationForm
+            defaultValues={{
+              universityName: '',
+              majorName: '',
+              admissionYear: new Date().getFullYear(),
+              graduationYear: null,
+              degreeName: '졸업',
+            }}
+            onSubmit={handleFormSubmit}
+            onCancel={() => {}}
+            isEditMode={false}
+          />
         )}
 
         {/* Footer */}
@@ -304,12 +156,7 @@ export default function RegisterSchool() {
             </Link>
 
             <Link href="/onBoarding/person/career">
-              <button
-                className={`rounded bg-[#2563EB] px-16 
-                py-2 text-[#fff] `}
-              >
-                다음
-              </button>
+              <button className="rounded bg-[#2563EB] px-16 py-2 text-[#fff]">다음</button>
             </Link>
           </div>
         </div>
