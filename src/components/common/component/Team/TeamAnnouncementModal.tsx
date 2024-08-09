412c3f28da067d382b3b9503f8f9a36bfc4ddc0c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-09 16:31:52 +0900

    fix: 팀원공고 텍스트 길이가 길어지면 화면을 넘치는 현상 수정

--- src/components/common/component/Team/TeamAnnouncementModal.tsx
+++ src/components/common/component/Team/TeamAnnouncementModal.tsx
@@ -37,19 +37,19 @@ const TeamAnnouncementModal: React.FC<ModalProps> = ({ onClose, data }) => {
       className="fixed inset-0 z-50 flex items-center justify-center bg-[#000] bg-opacity-50"
       onClick={handleClickOutside}
     >
-      <div className="flex w-[47rem] flex-col rounded-lg bg-[#fff] p-6 shadow-lg">
-        <div className="flex justify-between">
+      <div className="flex h-[98%] w-[90%] flex-col overflow-hidden rounded-lg bg-[#fff] shadow-lg md:h-auto md:w-[47rem]">
+        <div className="flex justify-between p-6">
           <h1 className="text-lg font-semibold">팀원 공고</h1>
           <button onClick={onClose} className="text-xl">
             &times;
           </button>
         </div>
-        <div className="mt-4 rounded-[0.63rem] border border-grey30 p-5">
+        <div className="flex-1 overflow-auto p-6">
           <p className="text-sm text-grey60">{data.teamName}</p>
-          <h2 className=" pt-[0.44rem] font-semibold">{data.jobRoleName}</h2>
-          <div className="mt-[0.88rem] flex gap-2">
+          <h2 className="pt-[0.44rem] font-semibold">{data.jobRoleName}</h2>
+          <div className="mt-[0.88rem] flex flex-wrap gap-2">
             {data.skillNames.map((skill, index) => (
-              <span key={index} className="rounded-[0.31rem] border border-grey40 px-[0.88rem] py-1">
+              <span key={index} className="rounded-[0.31rem] border border-grey40 px-[0.88rem] py-1 text-sm">
                 {skill}
               </span>
             ))}
@@ -70,11 +70,8 @@ const TeamAnnouncementModal: React.FC<ModalProps> = ({ onClose, data }) => {
             </div>
           </div>
 
-          <div className="flex w-full justify-end">
-            <button
-              onClick={() => setIsMatchModalOpen(true)}
-              className="mt-6 rounded-lg bg-[#2563EB] px-4 py-2 text-[#fff]"
-            >
+          <div className="mt-6 flex w-full justify-end">
+            <button onClick={() => setIsMatchModalOpen(true)} className="rounded-lg bg-[#2563EB] px-4 py-2 text-[#fff]">
               지원하기
             </button>
           </div>
