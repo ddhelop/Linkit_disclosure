4c380308f912fed26387dc0e606cbf42ead4f31c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-09 20:38:10 +0900

    fix: 모바일 스크롤안되는 버그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${pretendard.variable}`}>
       <head>
-        <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </head>
       <body className={`${pretendard.className} bg-[#fff]`}>
         <ClientProvider>
