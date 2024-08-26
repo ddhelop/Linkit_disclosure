e940e544bf315f466216997ce5ff34df786e62f4
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-26 18:14:50 +0900

    fix: google - code public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         {/* google search console */}
-        <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_SITE_CONTENT || ''} />
+        <meta name="google-site-verification" content="PP9z5zXtPDA9QeD8TEvKvlzTGIDZt3R9tCpcUZfoFZ4" />
       </head>
       <body className={`${pretendard.className} bg-[#fff]`}>
         <ClientProvider>
