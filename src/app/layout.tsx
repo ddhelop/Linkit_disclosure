5b1a5cb7fabcdc9be55add95f8785f54b648131d
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-26 18:20:46 +0900

    feat: 네이버 서치 어드바이저 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,12 @@ import './globals.css'
 import Scripts from '@/components/script'
 
 import ClientProvider from '@/components/common/ClientProvider'
-import FetchSetting from '@/components/common/fetch/page'
 
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 
 export const metadata: Metadata = {
-  title: 'Linkit',
+  title: '링킷, Linkit',
   description: '팀 빌딩 서비스, 링킷',
   icons: {
     icon: '/logo.png',
@@ -56,8 +55,9 @@ export default function RootLayout({
     <html lang="ko" className={`${pretendard.variable}`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-        {/* google search console */}
+
         <meta name="google-site-verification" content="PP9z5zXtPDA9QeD8TEvKvlzTGIDZt3R9tCpcUZfoFZ4" />
+        <meta name="naver-site-verification" content="17163296e94f835f71bb3d1994ea168bd456675b" />
       </head>
       <body className={`${pretendard.className} bg-[#fff]`}>
         <ClientProvider>
