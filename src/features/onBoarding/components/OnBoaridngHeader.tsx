c9cd5144aa4ba8f6126e984f42d1a83138819fee
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-10-28 14:52:10 +0900

    feat: 온보딩 헤더의 로고 라우팅 추가(#12)

--- src/features/onBoarding/components/OnBoaridngHeader.tsx
+++ src/features/onBoarding/components/OnBoaridngHeader.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function OnBoaridngHeader() {
   return (
     <>
-      <nav className="flex h-auto w-full px-10 py-6">
-        <Image src="/common/icons/blue_logo_row.svg" alt="LinKit Logo" width={115} height={22} />
+      <nav className="flex h-auto w-full px-10 py-4">
+        <Link href="/">
+          <Image src="/common/icons/blue_logo_row.svg" alt="LinKit Logo" width={115} height={22} />
+        </Link>
       </nav>
     </>
   )
