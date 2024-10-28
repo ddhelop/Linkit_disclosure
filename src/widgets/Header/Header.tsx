f30087194a81b27662a994bc8d9c3fa6a2efda53
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-10-28 17:43:52 +0900

    fix: 헤더창 따라 내려오는 버그 해결 sticky

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -2,19 +2,15 @@
 'use client'
 
 import { useMobileMenu } from './hooks/useMobileMenu'
-
 import { useAuthHandler } from './hooks/useAuthHandler'
 import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
-  // const { mobileMenuOpen, toggleMobileMenu, closeMobileMenu } = useMobileMenu()
-  // const { isAuth, isLoginModalOpen, openLoginModal, closeLoginModal, handleLogout } = useAuthHandler()
-
   const pathname = usePathname()
 
-  // 📌 특정 경로에서 Header를 숨기기
+  // 특정 경로에서 Header를 숨기기
   const hideHeaderOnPaths = ['/login/onboarding-info', '/login/onboarding-agree', '/login/onboarding-complete']
   const basePath = pathname.split('?')[0] // 쿼리 파라미터 제거된 경로 확인
 
@@ -24,15 +20,17 @@ export default function Header() {
 
   return (
     <>
-      <header className="relative z-[100] flex h-[4.3rem] w-screen shrink-0 items-center justify-between px-10">
+      <header className="sticky top-0 z-[100] flex h-[4.3rem] w-full items-center justify-between bg-white px-10">
         <div className="flex h-full items-center">
-          <Image
-            src="/common/icons/blue_logo_row.svg"
-            alt="Linkit"
-            width={115}
-            height={22}
-            className="cursor-pointer"
-          />
+          <Link href="/">
+            <Image
+              src="/common/icons/blue_logo_row.svg"
+              alt="Linkit"
+              width={115}
+              height={22}
+              className="cursor-pointer"
+            />
+          </Link>
           <div className="ml-12 flex h-full items-center text-grey60">
             <Link
               href="/"
@@ -40,7 +38,6 @@ export default function Header() {
             >
               팀원
             </Link>
-
             <Link
               href="/"
               className="mt-2 flex items-center border-b-2 border-transparent px-7 pb-2 hover:border-main hover:text-grey100"
@@ -63,9 +60,11 @@ export default function Header() {
           <Link className="px-7" href={''}>
             FAQ
           </Link>
-          <button className="rounded-[1.38rem] bg-[#D3E1FE] px-[1.62rem] py-[0.38rem] font-semibold transition-colors duration-100 hover:bg-main hover:text-white hover:ring-4">
-            로그인
-          </button>
+          <Link href="/login">
+            <button className="rounded-[1.38rem] bg-[#D3E1FE] px-[1.62rem] py-[0.38rem] font-semibold transition-colors duration-100 hover:bg-main hover:text-white hover:ring-4">
+              로그인
+            </button>
+          </Link>
         </div>
       </header>
     </>
