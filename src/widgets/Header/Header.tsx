8ada9dba16887e632d7f7b1843a5824fb480459d
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-10-28 17:19:26 +0900

    refactor: 헤더 변경 !

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -3,17 +3,14 @@
 
 import { useMobileMenu } from './hooks/useMobileMenu'
 
-import NavigationLinks from './components/NavigationLinks'
-import MobileMenu from './components/MobileMenu'
-import AccountModal from '@/components/common/user/AccountModal'
-import PopUpAlertModal from '@/components/common/CommonModal/PopUpAlertModal'
-import LoginModal from '@/features/login/components/LoginModal'
 import { useAuthHandler } from './hooks/useAuthHandler'
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
-  const { mobileMenuOpen, toggleMobileMenu, closeMobileMenu } = useMobileMenu()
-  const { isAuth, isLoginModalOpen, openLoginModal, closeLoginModal, handleLogout } = useAuthHandler()
+  // const { mobileMenuOpen, toggleMobileMenu, closeMobileMenu } = useMobileMenu()
+  // const { isAuth, isLoginModalOpen, openLoginModal, closeLoginModal, handleLogout } = useAuthHandler()
 
   const pathname = usePathname()
 
@@ -27,35 +24,50 @@ export default function Header() {
 
   return (
     <>
-      <nav className="fixed top-0 z-[20] w-full flex-shrink bg-white backdrop-blur-3xl">
-        <div className="flex w-full items-center justify-between px-[2.5rem] py-[1.3rem]">
-          <NavigationLinks isAuth={isAuth} openLoginModal={openLoginModal} />
-
-          <div className="ml-auto flex md:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex cursor-pointer items-center justify-center rounded-md p-2.5 text-grey100"
-              onClick={toggleMobileMenu}
+      <header className="relative z-[100] flex h-[4.3rem] w-screen shrink-0 items-center justify-between px-10">
+        <div className="flex h-full items-center">
+          <Image
+            src="/common/icons/blue_logo_row.svg"
+            alt="Linkit"
+            width={115}
+            height={22}
+            className="cursor-pointer"
+          />
+          <div className="ml-12 flex h-full items-center text-grey60">
+            <Link
+              href="/"
+              className="mt-2 flex items-center border-b-2 border-transparent px-7 pb-2 hover:border-main hover:text-grey100"
             >
-              {mobileMenuOpen ? (
-                <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-              ) : (
-                <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-              )}
-            </button>
+              팀원
+            </Link>
+
+            <Link
+              href="/"
+              className="mt-2 flex items-center border-b-2 border-transparent px-7 pb-2 hover:border-main hover:text-grey100"
+            >
+              팀
+            </Link>
+            <Link
+              href="/"
+              className="mt-2 flex items-center border-b-2 border-transparent px-7 pb-2 hover:border-main hover:text-grey100"
+            >
+              모집 공고
+            </Link>
           </div>
         </div>
 
-        <MobileMenu isAuth={isAuth} handleLogout={handleLogout} isOpen={mobileMenuOpen} closeMenu={closeMobileMenu} />
-      </nav>
-
-      <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
-      <AccountModal onClose={() => {}} />
-      <PopUpAlertModal isOpen={false} onClose={() => {}} text="곧 오픈 예정입니다!" />
+        <div className="flex items-center font-normal text-grey90">
+          <Link className="px-7" href={''}>
+            ABOUT US
+          </Link>
+          <Link className="px-7" href={''}>
+            FAQ
+          </Link>
+          <button className="rounded-[1.38rem] bg-[#D3E1FE] px-[1.62rem] py-[0.38rem] font-semibold transition-colors duration-100 hover:bg-main hover:text-white hover:ring-4">
+            로그인
+          </button>
+        </div>
+      </header>
     </>
   )
 }
