5ae5667db94e2f9e2723c799849f8837d35ec02a
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-10-31 09:56:57 +0900

    fix: 로그인시 바로 UI 렌더링 안되는 버그 수정 (#13)

--- src/features/login/model/useNaverAuth.ts
+++ src/features/login/model/useNaverAuth.ts
@@ -5,10 +5,11 @@ import { naverLogin } from '../api/authApi'
 import { LoginResponse } from './authType'
 
 import { setCookie } from 'cookies-next'
+import { useUserStore } from '@/shared/store/useAuthStore'
 
 export const useKakaoAuth = (code: string | null) => {
   const router = useRouter()
-
+  const { checkLogin } = useUserStore()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -21,7 +22,7 @@ export const useKakaoAuth = (code: string | null) => {
         if (isMemberBasicInform) {
           // 기존 회원: Recoil 상태에 토큰 저장 후 홈으로 이동
           setCookie('access-token', accessToken)
-
+          checkLogin()
           router.push('/')
         } else {
           // 신규 회원: 세션 스토리지에 토큰 저장 후 온보딩 페이지로 이동
