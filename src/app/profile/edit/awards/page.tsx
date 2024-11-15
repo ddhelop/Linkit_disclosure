f20723e5b2f9cbdb55a83ee32f9b649d95c17411
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-11-15 13:09:23 +0900

    refactor: 이력 컴포넌트화

---
+++ src/app/profile/edit/awards/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfileEditAwards() {
+  return (
+    <div className="pl-[4.25rem] pr-[8.69rem] pt-[3.75rem]">
+      <label className="text-xl font-bold">수상</label>
+
+      <div className="mt-5 rounded-xl"></div>
+    </div>
+  )
+}
--- src/features/profile/edit/components/ProfileEditHistory.tsx
+++ src/features/profile/edit/components/ProfileEditHistory.tsx
@@ -1,14 +1,17 @@
+import { Button } from '@/shared/ui/Button/Button'
+import Image from 'next/image'
+import ElementComponent from './common/ElementComponent'
+
 export default function ProfileEditHistory() {
   return (
     <>
-      <div className="flex flex-col rounded-xl bg-white px-[1.62rem] pb-7 pt-2">
-        <div className="flex flex-col gap-1 px-[1.62rem] py-4">
-          <span className="font-semibold text-grey80">리에종</span>
-          <div className="flex gap-4 text-xs">
-            <span className="text-grey80">Frontend Developer</span>
-            <span className="text-grey60">2022.06. ~ 2026.06</span>
-          </div>
-        </div>
+      <div className="flex flex-col rounded-xl bg-white px-[1.62rem] pb-7 pt-[1.87rem]">
+        <Button mode="main2" animationMode="main">
+          활동 추가하기
+        </Button>
+
+        {/*  */}
+        <ElementComponent />
       </div>
     </>
   )
