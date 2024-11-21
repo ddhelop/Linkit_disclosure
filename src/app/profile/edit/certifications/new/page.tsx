4027272b1252ebe5645a64a8138edec88e07bd5f
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-11-21 22:02:11 +0900

    style: 자격증 컴포넌트 뒤로가기 버튼 추가

--- src/app/profile/edit/certifications/new/page.tsx
+++ src/app/profile/edit/certifications/new/page.tsx
@@ -1,9 +1,16 @@
 import NewCertificate from '@/features/profile/edit/components/New/NewCertificate'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function ProfileEditNewCertificationsPage() {
   return (
     <div className="pl-[4.25rem] pr-[8.69rem] pt-[3.75rem]">
-      <label className="text-xl font-bold">자격증</label>
+      <div className="flex items-center gap-2">
+        <Link href="/profile/edit/certifications">
+          <Image src="/common/icons/arrow-left.svg" alt="back" width={24} height={24} />
+        </Link>
+        <label className="text-xl font-bold">자격증</label>
+      </div>
 
       <div className="mt-5 rounded-xl">
         <NewCertificate />
