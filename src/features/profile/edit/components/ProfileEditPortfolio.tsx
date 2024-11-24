d28f1be77d195c8e0549b3d8e7f701e5211abb53
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-11-24 16:13:19 +0900

    feat: ProfileEdit : 포트폴리오 리스트 조회

--- src/features/profile/edit/components/ProfileEditPortfolio.tsx
+++ src/features/profile/edit/components/ProfileEditPortfolio.tsx
@@ -1,9 +1,28 @@
+'use client'
+
 import { Button } from '@/shared/ui/Button/Button'
 import Image from 'next/image'
 import ProjectComponent from './common/ProjectComponent'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { getPortfolioItems, PortfolioItem } from '../../api/getPortfolioItems'
 
 export default function ProfileEditPortfolio() {
+  const [portfolioItems, setPortfolioItems] = useState<PortfolioItem[]>([])
+
+  useEffect(() => {
+    const fetchPortfolioItems = async () => {
+      try {
+        const items = await getPortfolioItems()
+        setPortfolioItems(items)
+      } catch (error) {
+        console.error('포트폴리오 데이터 조회 실패:', error)
+      }
+    }
+
+    fetchPortfolioItems()
+  }, [])
+
   return (
     <>
       <div className="flex flex-col rounded-xl bg-white px-[1.62rem] pb-7 pt-[1.88rem]">
@@ -11,15 +30,16 @@ export default function ProfileEditPortfolio() {
           <Button
             animationMode="main"
             mode="main2"
-            className=" flex w-full items-center justify-center gap-2 py-[0.38rem] text-sm"
+            className="flex w-full items-center justify-center gap-2 py-[0.38rem] text-sm"
           >
             <Image src="/common/icons/plus.svg" width={15} height={15} alt="plus-icon" />새 프로젝트
           </Button>
         </Link>
 
-        {/* 프로젝트 */}
-        <div className="flex flex-col pt-6">
-          <ProjectComponent />
+        <div className="flex flex-col gap-4 pt-6">
+          {portfolioItems.map((item) => (
+            <ProjectComponent key={item.profilePortfolioId} {...item} />
+          ))}
         </div>
       </div>
     </>
--- src/features/profile/edit/components/common/ProjectComponent.tsx
+++ src/features/profile/edit/components/common/ProjectComponent.tsx
@@ -1,6 +1,25 @@
 import Image from 'next/image'
 
-export default function ProjectComponent() {
+interface ProjectComponentProps {
+  projectName: string
+  projectSize: 'PERSONAL' | 'TEAM'
+  projectLineDescription: string
+  projectStartDate: string
+  projectEndDate: string
+  isProjectInProgress: boolean
+  projectRoles: string[]
+  projectRepresentImagePath: string
+}
+
+export default function ProjectComponent({
+  projectName,
+  projectSize,
+  projectLineDescription,
+  projectStartDate,
+  projectEndDate,
+  projectRoles,
+  projectRepresentImagePath,
+}: ProjectComponentProps) {
   return (
     <div className="relative flex gap-6 rounded-xl bg-grey10 px-6 py-7">
       <Image
@@ -10,22 +29,28 @@ export default function ProjectComponent() {
         alt="more"
         className="absolute right-5 top-5 cursor-pointer"
       />
-      <Image src="/common/images/no_thumbnail.svg" width={256} height={144} alt="no_thumbnail" />
+      <Image
+        src={projectRepresentImagePath || '/common/images/no_thumbnail.svg'}
+        width={256}
+        height={144}
+        alt="thumbnail"
+      />
       <div className="flex flex-col justify-center">
         <div className="flex items-center gap-3">
-          <span className="text-lg font-semibold">프로젝트명</span>
-          <span className="flex items-center rounded-[62.5rem] bg-[#D3E1FE] px-[0.88rem] text-xs">팀</span>
+          <span className="text-lg font-semibold">{projectName}</span>
+          <span className="flex items-center rounded-[62.5rem] bg-[#D3E1FE] px-[0.88rem] text-xs">
+            {projectSize === 'TEAM' ? '팀' : '개인'}
+          </span>
         </div>
 
-        <div className="mt-3 text-xs font-normal text-grey60">
-          한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개
-        </div>
+        <div className="mt-3 text-xs font-normal text-grey60">{projectLineDescription}</div>
 
         <div className="mt-[1.12rem] flex gap-1 text-xs text-grey70">
-          <span className="text-grey60">기간 | </span>2024.06 ~ 2024.11
+          <span className="text-grey60">기간 | </span>
+          {projectStartDate} ~ {projectEndDate}
         </div>
         <div className="mt-2 flex gap-1 text-xs text-grey70">
-          <span className="text-grey60">역할 |</span> 컨텐츠 제작, 브랜딩, 계정 관리
+          <span className="text-grey60">역할 |</span> {projectRoles.join(', ')}
         </div>
       </div>
     </div>
