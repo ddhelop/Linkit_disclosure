17c9f9d3082433a2789c8542ae9f1909586a3510
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-09 14:34:14 +0900

    feat: 팀 프로필 : 팀 정보가 있을때 팀정보 UI

--- src/features/team/view/common/TeamViewClient.tsx
+++ src/features/team/view/common/TeamViewClient.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/shared/ui/Button/Button'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function TeamViewClient() {
@@ -9,7 +10,8 @@ export default function TeamViewClient() {
         className="flex h-[18.4rem] px-[7.12rem] py-8"
         style={{ background: 'linear-gradient(180deg, #D3E1FE -16.67%, #FCFCFD 100%)' }}
       >
-        <div
+        {/* 팀 정보가 없을 때 */}
+        {/* <div
           className="flex h-full w-full flex-col items-center justify-center rounded-[1.875rem] bg-white bg-opacity-50"
           style={{ boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)' }}
         >
@@ -21,6 +23,60 @@ export default function TeamViewClient() {
               팀 생성하러 가기
             </Button>
           </Link>
+        </div> */}
+
+        {/* 팀 정보가 있을 때 */}
+        <div className="flex w-full justify-between">
+          {/* 왼쪽 */}
+          <div className="flex flex-col">
+            <div className="flex gap-2">
+              <span className="rounded-[0.38rem] bg-[#4D82F3] px-2 py-1 text-xs text-white">팀 찾는중</span>
+              <span className="rounded-[0.38rem] bg-[#4D82F3] px-2 py-1 text-xs text-white">팀원 찾는중</span>
+              <span className="rounded-[18.38rem] bg-[#D3E1FE] px-2 py-1 text-xs text-main2">+2</span>
+            </div>
+
+            {/* 팀 정보 */}
+            <div className="mt-5 flex justify-center">
+              <div className="flex gap-8">
+                <Image src="/common/default_profile.svg" alt="default-profile" width={132} height={132} />
+                <div className="flex flex-col justify-center">
+                  <div className="flex items-center gap-3">
+                    <h1 className="text-2xl font-bold text-grey90">팀이름</h1>
+                    <span className="text-xs text-grey70">스크랩 수 : 50</span>
+                  </div>
+
+                  <div className="mt-2 flex flex-col gap-1">
+                    <span className="flex gap-2 text-xs text-grey50">
+                      팀원 | <span className="text-grey70">5인</span>
+                    </span>
+                    <span className="flex gap-2 text-xs text-grey50">
+                      지역 | <span className="text-grey70">서울시 마포구</span>
+                    </span>
+
+                    <span className="mt-2 text-xs text-grey70">
+                      한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개한줄소개...
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 오른쪽 버튼들 */}
+
+          <div className="flex flex-col justify-center gap-[1.13rem]">
+            <Link
+              href="/team/edit"
+              className="flex h-[3rem] w-[20rem] items-center justify-center rounded-[62.5rem] bg-[#D3E1FE] font-semibold text-main2"
+            >
+              팀관리
+            </Link>
+            <Link
+              href="/team/edit"
+              className="flex h-[3rem] w-[20rem] items-center justify-center rounded-[62.5rem] bg-[#D3E1FE] font-semibold text-main2"
+            >
+              팀 추가하기
+            </Link>
+          </div>
         </div>
       </div>
 
