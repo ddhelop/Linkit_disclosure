a0fc6b7d262b49529b6ee99ae92bc5416101bb88
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-17 21:54:05 +0900

    feat: 홈화면 ui (~나를 찾는 팀이 있어요!)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,21 @@
 'use client'
 
+import MiniTeamCard from '@/shared/components/MiniTeamCard'
 import Banner from '../Banner/Banner'
 
 export default function Home() {
-  return <Banner />
+  return (
+    <div className="flex w-full flex-col items-center bg-white">
+      <Banner />
+
+      <div className="mt-[3.77rem] flex w-[65%] flex-col gap-4">
+        <h1 className="text-xl font-semibold">나를 찾는 팀이 있어요!</h1>
+
+        <div className="flex gap-9">
+          <MiniTeamCard />
+          <MiniTeamCard />
+        </div>
+      </div>
+    </div>
+  )
 }
