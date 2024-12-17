bfb5b88b85cfc43d17fa0214db2a721b8274cfb5
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-17 11:40:17 +0900

    feat: 팀 프로필 설정 : 연혁 리스트 단일 항목 ui

--- src/app/team/123/edit/history/page.tsx
+++ src/app/team/123/edit/history/page.tsx
@@ -6,12 +6,14 @@ export default function TeamEditHistoryPage() {
   return (
     <div className="flex flex-col">
       <h1 className="text-xl font-bold">연혁</h1>
-      <TeamEditHistoy />
+
       <Link href="/team/edit/123/history/new">
         <Button mode="main2" animationMode="main" className="mt-5 w-full rounded-[0.69rem] py-2">
           + 추가하기
         </Button>
       </Link>
+
+      <TeamEditHistoy />
     </div>
   )
 }
--- src/features/team/edit/history/TeamEditHistoy.tsx
+++ src/features/team/edit/history/TeamEditHistoy.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+
 export default function TeamEditHistoy() {
   return (
-    <div>
-      <></>
+    <div className="mt-5 flex flex-col gap-5">
+      <div className="rounded-xl bg-white px-10 py-5">
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
+            <span className="font-semibold text-grey80">연혁명연혁명</span>
+            <span className="text-xs font-normal text-grey60">2024.10~2024.11</span>
+          </div>
+          <Image src="/common/icons/more_row.svg" alt="delete" width={20} height={20} className="cursor-pointer" />
+        </div>
+      </div>
     </div>
   )
 }
