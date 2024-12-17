d05d6d54dbfd47c77d3900f01bbf6339f73a5d68
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-17 10:38:53 +0900

    feat: 팀 프로필 설정 : 팀원 List component ui

--- src/app/team/123/edit/members/page.tsx
+++ src/app/team/123/edit/members/page.tsx
@@ -1,4 +1,4 @@
-import TeamEditMembers from '@/features/team/edit/members/TeamEditMembers'
+import TeamEditMembersList from '@/features/team/edit/members/TeamEditMembersList'
 import { Button } from '@/shared/ui/Button/Button'
 import Link from 'next/link'
 
@@ -6,12 +6,14 @@ export default function TeamEditMembersPage() {
   return (
     <div className="flex flex-col">
       <h1 className="text-xl font-bold">팀 구성원</h1>
-      <TeamEditMembers />
+
       <Link href="/team/edit/123/members/new">
         <Button mode="main2" animationMode="main" className="mt-5 w-full rounded-[0.69rem] py-2">
           + 추가하기
         </Button>
       </Link>
+
+      <TeamEditMembersList />
     </div>
   )
 }
--- src/features/team/edit/members/TeamEditMembers.tsx
+++
@@ -1,7 +0,0 @@
-export default function TeamEditMembers() {
-  return (
-    <div>
-      <></>
-    </div>
-  )
-}
