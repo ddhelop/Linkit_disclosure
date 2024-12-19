49aeb35cd08418c0a700629fbab980b4e79ee317
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-19 10:15:06 +0900

    style:

--- src/features/team/edit/common/TeamEditLeftMenu.tsx
+++ src/features/team/edit/common/TeamEditLeftMenu.tsx
@@ -1,13 +1,13 @@
 // LeftMenu.tsx
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import TeamEditProfileCard from './TeamEditProfileCard'
 
 const menuItems: { label: string; path: string; subPaths?: string[] }[] = [
   { label: '팀 로그', path: '/team/123/edit/log' },
   { label: '기본 정보', path: '/team/123/edit/basic' },
-  { label: '모집 공고', path: '/team/123/edit/recruitment' },
+  { label: '모집 공고', path: '/team/123/edit/recruit' },
   {
     label: '팀 구성원',
     path: '/team/123/edit/members',
@@ -21,6 +21,9 @@ const menuItems: { label: string; path: string; subPaths?: string[] }[] = [
 
 const TeamEditLeftMenu = () => {
   const router = useRouter()
+  const pathname = usePathname()
+
+  const currentSection = pathname.split('/').pop()
 
   const handleNavigation = (path: string) => {
     router.push(path)
@@ -28,18 +31,19 @@ const TeamEditLeftMenu = () => {
 
   return (
     <div className="mt-6 w-[17.5rem]">
-      {/* 왼쪽 메뉴바 - 프로필 관리 */}
       <div className="flex w-full flex-col rounded-xl bg-[#FCFCFD]">
         <TeamEditProfileCard />
-        <ul className="mt-8 flex w-full flex-col items-end gap-4 ">
+        <ul className="mt-8 flex w-full flex-col items-end gap-4">
           {menuItems.map((item, index) => {
+            const isSelected = item.path.includes(currentSection ?? '')
             return (
               <li
                 key={index}
                 onClick={() => handleNavigation(item.path)}
-                className={`relative flex w-full cursor-pointer items-center justify-between rounded-lg py-2 pl-6  hover:bg-grey20`}
+                className={`relative flex w-full cursor-pointer items-center justify-between rounded-lg py-2 pl-6 
+                  ${isSelected ? 'bg-grey20 font-semibold text-grey90' : 'hover:bg-grey20'}`}
               >
-                <span className="text-sm text-grey80">{item.label}</span>
+                <span className={`text-sm ${isSelected ? 'text-grey90' : 'text-grey80'}`}>{item.label}</span>
               </li>
             )
           })}
