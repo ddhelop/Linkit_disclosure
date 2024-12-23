879c853fadbaff3d174d991cd682ef6956db25c8
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-23 15:21:32 +0900

    fix: 팀 연혁 수정 라우팅(팀이름 적용) 경로 수정

--- src/app/team/[teamName]/edit/history/new/page.tsx
+++ src/app/team/[teamName]/edit/history/new/page.tsx
@@ -2,10 +2,10 @@ import TeamEditHistoryNew from '@/features/team/edit/history/\bTeamEditHistoryNe
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function TeamEditHistoryNewPage() {
+export default function TeamEditHistoryNewPage({ params }: { params: { teamName: string } }) {
   return (
     <div className="flex flex-col gap-5">
-      <Link href="/team/123/edit/products" className="flex gap-2">
+      <Link href={`/team/${params.teamName}/edit/history`} className="flex gap-2">
         <Image src="/common/icons/arrow-left.svg" width={24} height={24} alt="back" />
         <label className="text-xl font-bold">연혁</label>
       </Link>
--- src/app/team/[teamName]/edit/history/page.tsx
+++ src/app/team/[teamName]/edit/history/page.tsx
@@ -2,12 +2,12 @@ import TeamEditHistoy from '@/features/team/edit/history/TeamEditHistoy'
 import { Button } from '@/shared/ui/Button/Button'
 import Link from 'next/link'
 
-export default function TeamEditHistoryPage() {
+export default function TeamEditHistoryPage({ params }: { params: { teamName: string } }) {
   return (
     <div className="flex flex-col">
       <h1 className="text-xl font-bold">연혁</h1>
 
-      <Link href="/team/123/edit/history/new">
+      <Link href={`/team/${params.teamName}/edit/history/new`}>
         <Button mode="main2" animationMode="main" className="mt-5 w-full rounded-[0.69rem] py-2">
           + 추가하기
         </Button>
