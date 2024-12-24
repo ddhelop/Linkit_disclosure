c38572f46f8caf8d204377cf18c914f628ec27b0
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-24 11:45:58 +0900

    feat: 팀 뷰어 : 로그 리스트

--- src/app/team/[teamName]/(view)/log/(view)/list/page.tsx
+++ src/app/team/[teamName]/(view)/log/(view)/list/page.tsx
@@ -2,7 +2,7 @@ import TeamViewLogList from '@/features/team/view/log/TeamViewLogList'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function TeamViewLogDetailPage({ params }: { params: { teamName: string } }) {
+export default function TeamViewLogListPage({ params }: { params: { teamName: string } }) {
   return (
     <div className="flex flex-col gap-6">
       <div className="flex items-center gap-2">
--- src/app/team/[teamName]/(view)/log/page.tsx
+++ src/app/team/[teamName]/(view)/log/page.tsx
@@ -1,9 +1,26 @@
 import TeamViewLog from '@/features/team/view/log/TeamViewLog'
+import { Button } from '@/shared/ui/Button/Button'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function TeamLogPage({ params }: { params: { teamName: string } }) {
   return (
-    <div className="pt-[3rem]">
+    <div className="flex flex-col pt-[3rem]">
       <TeamViewLog params={params} />
+
+      <div className="mt-10 flex justify-center">
+        <Link href={`/team/${params.teamName}/log/list`}>
+          <Button
+            mode="custom"
+            animationMode="grey"
+            size="custom"
+            className="flex items-center gap-2 rounded-full border border-grey30 bg-white px-6 py-2 text-sm text-grey80"
+          >
+            <span>팀이름의 로그 더보기</span>
+            <Image src={'/common/icons/arrow-right(greyblack).svg'} alt="arrow-right" width={22} height={22} />
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
