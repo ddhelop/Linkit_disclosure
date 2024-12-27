88fedf07169a35ccfddc5f1ad5b3f1912b06370a
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-12-27 13:57:26 +0900

    feat: 팀 뷰어 : 모집 공고 필터링

--- src/features/team/view/recruitment/TeamViewRecruitment.tsx
+++ src/features/team/view/recruitment/TeamViewRecruitment.tsx
@@ -1,29 +1,71 @@
 'use client'
 import { useEffect, useState } from 'react'
-import TeamViewNotView from '../common/TeamViewNotView'
+
 import { getTeamAnnouncements, TeamAnnouncement } from '../../api/teamApi'
 import TeamViewReruitComponent from './TeamViewReruitComponent'
 
 export default function TeamViewRecruitment({ teamName }: { teamName: string }) {
-  const [announcements, setAnnouncements] = useState<TeamAnnouncement[]>([])
+  const [data, setData] = useState<TeamAnnouncement[] | null>(null)
+  const [filter, setFilter] = useState<'ALL' | 'IN_PROGRESS' | 'CLOSED'>('ALL')
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await getTeamAnnouncements(teamName)
-      setAnnouncements(response.result.teamMemberAnnouncementItems)
+      setData(response.result.teamMemberAnnouncementItems)
     }
     fetchData()
   }, [teamName])
+
+  const filteredAnnouncements = data?.filter((announcement) => {
+    switch (filter) {
+      case 'IN_PROGRESS':
+        return announcement.isAnnouncementInProgress
+      case 'CLOSED':
+        return !announcement.isAnnouncementInProgress
+      default:
+        return true
+    }
+  })
+
   return (
     <>
-      {announcements.length > 0 ? (
-        <div className="mt-12 flex flex-col gap-6 pb-10">
-          {announcements.map((announcement) => (
+      <div className="mt-12 flex gap-3">
+        <div
+          className={`cursor-pointer rounded-full px-6 py-2 ${
+            filter === 'ALL' ? 'bg-[#4871E6] text-white' : 'bg-[#D3E1FE] text-grey60'
+          }`}
+          onClick={() => setFilter('ALL')}
+        >
+          전체
+        </div>
+        <div
+          className={`cursor-pointer rounded-full px-6 py-2 ${
+            filter === 'IN_PROGRESS' ? 'bg-[#4871E6] text-white' : 'bg-[#D3E1FE] text-grey60'
+          }`}
+          onClick={() => setFilter('IN_PROGRESS')}
+        >
+          진행 중
+        </div>
+        <div
+          className={`cursor-pointer rounded-full px-6 py-2 ${
+            filter === 'CLOSED' ? 'bg-[#4871E6] text-white' : 'bg-[#D3E1FE] text-grey60'
+          }`}
+          onClick={() => setFilter('CLOSED')}
+        >
+          마감
+        </div>
+      </div>
+      {filteredAnnouncements && filteredAnnouncements.length > 0 ? (
+        <div className="mt-8 flex flex-col gap-6 pb-10">
+          {filteredAnnouncements.map((announcement) => (
             <TeamViewReruitComponent key={announcement.teamMemberAnnouncementId} announcement={announcement} />
           ))}
         </div>
       ) : (
-        <div className="">
-          <TeamViewNotView />
+        <div className="mt-8 text-center text-grey60">
+          {filter === 'IN_PROGRESS' && '진행 중인 공고가 없어요!'}
+          {filter === 'CLOSED' && '마감된 공고가 없어요!'}
+          {filter === 'ALL' && '등록된 공고가 없어요!'}
         </div>
       )}
     </>
