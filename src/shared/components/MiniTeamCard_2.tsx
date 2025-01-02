4c067f11340d6a6c3c2c6ea55c216db6b1a8c551
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-02 17:55:50 +0900

    feat: TeamMiniCard_2 UI

---
+++ src/shared/components/MiniTeamCard_2.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+
+export default function MiniTeamCard_2() {
+  return (
+    <div
+      className="flex flex-col rounded-xl px-7 py-[1.12rem] hover:border hover:border-[#7EA5F8]"
+      style={{ boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)' }}
+    >
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <div className="rounded-[0.38rem] bg-[#EDF3FF] px-2 py-1 text-xs text-[#3774F4]">팀찾는중</div>
+          <div className="rounded-[0.38rem] bg-[#EDF3FF] px-2 py-1 text-xs text-[#3774F4]">팀찾는중</div>
+          <div className="rounded-[0.38rem] bg-[#EDF3FF] px-2 py-1 text-xs text-[#3774F4]">+2</div>
+        </div>
+        <Image src="/common/icons/save.svg" width={16} height={16} alt="save" />
+      </div>
+
+      {/*  */}
+      <div className="mt-5 flex gap-4">
+        <Image src="/common/default_profile.svg" width={70} height={70} alt="profile" />
+        <div className="flex flex-col justify-center gap-1">
+          <div className="flex items-center gap-3">
+            <span className="text-lg font-bold text-grey90">팀이름</span>
+            <span className="text-xs text-grey70">스크랩 수 50</span>
+          </div>
+          <span className="flex gap-1 text-xs text-grey50">
+            <span>팀원 </span>
+            <span>|</span>
+            <span className="text-grey70">5인</span>
+          </span>
+          <span className="flex gap-1 text-xs text-grey50">
+            <span>지역 </span>
+            <span>|</span>
+            <span className="text-grey70">서울시 마포구</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
