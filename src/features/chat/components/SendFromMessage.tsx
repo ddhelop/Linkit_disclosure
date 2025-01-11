ae3fc49cac13040384f189a0a3104fec8c3bd2b0
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-11 14:07:18 +0900

    style: 메세지 컴포넌트 UI

---
+++ src/features/chat/components/SendFromMessage.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+
+export default function SendFromMessage() {
+  return (
+    <>
+      {/* 상대방이 보낸 메세지 */}
+      <div className="mb-4 flex gap-3">
+        <Image
+          src="/common/default_profile.svg" // 실제 프로필 이미지 경로로 수정 필요
+          alt="프로필 이미지"
+          width={48}
+          height={48}
+          className="h-12 w-12 rounded-lg object-cover"
+        />
+        <div className="flex items-end gap-1">
+          <div className="max-w-[87%] rounded-2xl bg-[#fcfcfd] p-5">
+            <p className="text-sm text-grey80">
+              내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            </p>
+          </div>
+          <span className="mt-1 text-xs font-normal text-grey60">00:00</span>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/features/chat/components/SendToMessage.tsx
@@ -0,0 +1,17 @@
+export default function SendToMessage() {
+  return (
+    <div>
+      {/* 내가 보낸 메세지 */}
+      <div className="mb-4 flex w-full justify-end">
+        <div className="flex items-end justify-end gap-1">
+          <span className="mt-1 text-xs text-grey60">14:44</span>
+          <div className="max-w-[87%] rounded-2xl bg-grey30 p-5">
+            <p className="text-sm text-grey80">
+              내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
