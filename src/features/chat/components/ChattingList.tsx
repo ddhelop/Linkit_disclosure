4cb4a72de8172f4412cce1932d8e7aa6bf35e696
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-11 14:07:54 +0900

    style: 채팅방 리스트 UI

---
+++ src/features/chat/components/ChattingList.tsx
@@ -0,0 +1,9 @@
+import ChattingListComponent from './ChattingListComponent'
+
+export default function ChattingList() {
+  return (
+    <div className="flex w-[22.5rem] flex-col rounded-2xl border border-grey30 bg-white p-4">
+      <ChattingListComponent />
+    </div>
+  )
+}
