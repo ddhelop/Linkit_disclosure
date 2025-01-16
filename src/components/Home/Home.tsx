1a34a629911c1f1bc2c6e715d1e81f00285f7f58
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-16 10:59:15 +0900

    feat: 홈화면 - 팀 추천 섹션

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@
 
 import Banner from '../Banner/Banner'
 import HomeAnnouncementSection from './HomeAnnouncementSection'
+import HomeTeamSection from './HomeTeamSection'
 
 export default function Home() {
   return (
@@ -9,9 +10,12 @@ export default function Home() {
       <Banner />
 
       {/*  */}
-      <div className="mt-[3.77rem] flex w-[65%] flex-col items-center gap-4">
+      <div className="mt-[3.77rem] flex w-[65%] flex-col items-center gap-16">
         {/* 모집 공고 섹션 */}
         <HomeAnnouncementSection />
+
+        {/* 팀 섹션 */}
+        <HomeTeamSection />
       </div>
     </div>
   )
--- src/components/Home/api/HomeApi.ts
+++ src/components/Home/api/HomeApi.ts
@@ -1,8 +1,20 @@
 import { fetchWithAuth } from '@/shared/lib/api/fetchWithAuth'
 
+// 홈화면 - 모집 공고
 export const getAnnouncement = async () => {
   try {
     const response = await fetchWithAuth('/api/v1/home/announcement')
+    return response.json()
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+// 홈화면 - 팀 추천
+export const getTeamRecommend = async () => {
+  try {
+    const response = await fetchWithAuth('/api/v1/home/team')
     return response.json()
   } catch (error) {
     console.error(error)
