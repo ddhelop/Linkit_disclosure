13af679e1833875e4d361af31be7d55d55364fa7
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-19 15:45:23 +0900

    style: 팀원 공고 상세보기에서 뒤로가기 버튼 삭제

--- src/app/team/[teamName]/(view)/recruit/[id]/page.tsx
+++ src/app/team/[teamName]/(view)/recruit/[id]/page.tsx
@@ -1,17 +1,15 @@
 import TeamViewRecruitDetail from '@/features/team/view/recruitment/TeamViewRecruitDetail'
-import Image from 'next/image'
-import Link from 'next/link'
 
 export default function TeamViewRecruitDetailPage({ params }: { params: { teamName: string; id: string } }) {
   return (
     <div className="">
-      <div className="flex gap-2 p-4">
+      {/* <div className="flex gap-2 p-4">
         <Link href={`/team/${params.teamName}/recruit`} className="flex gap-2 rounded-lg p-1 hover:bg-grey20">
           <Image src="/common/icons/arrow-left.svg" alt="close" width={24} height={24} />
           <span className="text-xl font-semibold">{params.teamName}의 모집공고</span>
         </Link>
-      </div>
-      <div className="mt-6">
+      </div> */}
+      <div className="">
         <TeamViewRecruitDetail teamName={params.teamName} id={params.id} />
       </div>
     </div>
