5be71a59dc407e8e73fef0946d73f420a5e11811
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-20 18:04:43 +0900

    style: 로그컴포넌트 리스트 레이아웃 변경

--- src/app/team/[teamName]/(view)/log/(view)/layout.tsx
+++ src/app/team/[teamName]/(view)/log/(view)/layout.tsx
@@ -5,7 +5,7 @@ export default function TeamViewLogDetailLayout({ children }: { children: React.
         style={{ background: 'linear-gradient(180deg, #D3E1FE -16.67%, #FFFFFF 100%)' }}
         className="absolute left-0 flex h-[22.4rem] w-full"
       >
-        <div className=" w-full px-[16.12rem] py-[3.62rem]">{children}</div>
+        <div className=" w-full  py-[3.62rem]">{children}</div>
       </div>
     </div>
   )
--- src/app/team/[teamName]/(view)/log/(view)/list/page.tsx
+++ src/app/team/[teamName]/(view)/log/(view)/list/page.tsx
@@ -1,18 +1,33 @@
+import { getTeamInfo } from '@/features/team/api/teamApi'
 import TeamViewLogList from '@/features/team/view/log/TeamViewLogList'
+import MiniTeamCard from '@/shared/components/MiniTeamCard'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function TeamViewLogListPage({ params }: { params: { teamName: string } }) {
-  return (
-    <div className="flex flex-col gap-6">
-      <div className="flex items-center gap-2">
-        <Link href={`/team/${params.teamName}/log`}>
-          <Image src={'/common/icons/arrow-left.svg'} alt="arrow-left" width={24} height={24} />
-        </Link>
-        <h1 className="text-xl font-semibold">{params.teamName}의 로그</h1>
-      </div>
+export default async function TeamViewLogListPage({ params }: { params: { teamName: string } }) {
+  const teamInfo = await getTeamInfo(params.teamName)
 
-      <TeamViewLogList params={params} />
-    </div>
+  return (
+    <>
+      <div className="flex h-[calc(100vh-4rem)] w-full justify-center px-0">
+        <div className="flex w-[83%] flex-col items-center  gap-8">
+          {/* 헤더 */}
+          <div className="flex w-full gap-1">
+            <Link href={`/team/${params.teamName}/log`}>
+              <Image src={'/common/icons/arrow-left.svg'} alt="arrow-left" width={24} height={24} />
+            </Link>
+            <h1 className="text-xl font-semibold">{params.teamName}의 로그</h1>
+          </div>
+
+          <div className="flex w-full gap-8">
+            <TeamViewLogList params={params} />
+
+            <div>
+              <MiniTeamCard teamInfo={teamInfo} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
