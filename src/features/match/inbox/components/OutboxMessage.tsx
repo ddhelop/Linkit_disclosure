83132abdaa9b7ed0818bb3a2cf6e18508c63cf64
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-27 03:44:09 +0900

    style: 매칭관리 발신함 케이스별 UI 대응

---
+++ src/features/match/inbox/components/OutboxMessage.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import { MatchingMessage } from '../../types/MatchTypes'
+
+interface OutboxMessageProps {
+  message: MatchingMessage
+}
+
+export default function OutboxMessage({ message }: OutboxMessageProps) {
+  const isCompleted = message.matchingStatusType === 'COMPLETED'
+  const receiverInfo =
+    message.receiverType === 'TEAM' ? message.receiverTeamInformation : message.receiverProfileInformation
+
+  return (
+    <div className="w-full">
+      <div className="relative flex w-full gap-5 rounded-xl border border-grey30 bg-white px-10 py-7">
+        <div className="relative h-[64px] w-[64px] rounded-[0.63rem]">
+          <Image
+            src={
+              message.receiverType === 'TEAM'
+                ? message.receiverTeamInformation.teamLogoImagePath || '/common/default_profile.svg'
+                : message.receiverProfileInformation.profileImagePath || '/common/default_profile.svg'
+            }
+            alt="profile"
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex flex-col">
+          <span className={`text-lg font-semibold ${isCompleted ? 'text-main' : 'text-grey80'}`}>
+            {isCompleted
+              ? `${
+                  message.receiverType === 'TEAM'
+                    ? message.receiverTeamInformation.teamName + '팀'
+                    : message.receiverProfileInformation.memberName + '님'
+                }과 매칭 성사!`
+              : `${
+                  message.receiverType === 'TEAM'
+                    ? message.receiverTeamInformation.teamName + '팀'
+                    : message.receiverProfileInformation.memberName + '님'
+                }에게 매칭 요청 전송 완료`}
+          </span>
+          <span className="line-clamp-1 text-sm font-normal text-grey70">{message.requestMessage}</span>
+        </div>
+        <span className="absolute right-6 top-6 text-xs font-normal text-grey80">
+          {message.receiverType === 'TEAM'
+            ? `${message.receiverTeamInformation.teamName} · ${message.modifiedAt}`
+            : `${message.modifiedAt}`}
+        </span>
+      </div>
+    </div>
+  )
+}
