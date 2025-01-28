ec0590d9711e4f0d8dadf5c9cceac04f07f42a3c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-28 15:39:36 +0900

    feat: 홈화면 memberName 추가

--- src/components/Home/HomeAnnouncementSection.tsx
+++ src/components/Home/HomeAnnouncementSection.tsx
@@ -4,21 +4,26 @@ import AnnouncementCard from '@/shared/components/AnnouncementCard'
 import { useEffect, useState } from 'react'
 import { getAnnouncement } from './api/HomeApi'
 import { Announcement } from '@/features/find/types/FindTypes'
+import { IMyProfileBasicInform } from '@/shared/types/MyProfileBasicInformTypes'
+import { getMyProfileBasicInform } from '@/shared/api/profile/fetchProfileInform'
 
 export default function HomeAnnouncementSection() {
   const [announcement, setAnnouncement] = useState<Announcement[]>([])
+  const [profile, setProfile] = useState<IMyProfileBasicInform>()
 
   useEffect(() => {
     const fetchAnnouncement = async () => {
       const response = await getAnnouncement()
+      const profileResponse = await getMyProfileBasicInform()
       setAnnouncement(response.result.announcementInformMenus)
+      setProfile(profileResponse.result)
     }
     fetchAnnouncement()
   }, [])
 
   return (
     <div className="flex w-full flex-col gap-6">
-      <h1 className="text-xl font-semibold">뫄뫄님을 찾고 있어요!</h1>
+      <h1 className="text-xl font-semibold">{profile?.memberName}님을 찾고 있어요!</h1>
 
       <div className="grid grid-cols-3 gap-6">
         {announcement.map((announcement) => (
---
+++ src/shared/types/MyProfileBasicInformTypes.ts
@@ -0,0 +1,12 @@
+export interface IMyProfileBasicInform {
+  memberBasicInformId: number
+  memberName: string
+  contact: string
+  email: string
+  emailId: string
+  isServiceUseAgree: boolean
+  isPrivateInformAgree: boolean
+  isAgeCheck: boolean
+  isMarketingAgree: boolean
+  platform: string
+}
