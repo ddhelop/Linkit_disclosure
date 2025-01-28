599b663ba1f4cd76a7e0091273623f2a91376a3b
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-29 04:04:27 +0900

    chore: 인스타그램 버튼 깨짐 이슈

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer() {
             </Link>
             <Link href={'https://www.instagram.com/linkit_official'} target="_blank">
               <Image
-                src={'/common/icons/social/instagram_black.svg'}
+                src={'/common/icons/social/insta_black.svg'}
                 alt="instagram"
                 width={20}
                 height={20}
