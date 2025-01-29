c79aa88e049582a3addc9f299f79ac0383459d03
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-29 19:20:58 +0900

    feat: 채팅방 프로필에서 상대방 정보 표시

--- src/features/chat/components/ChattingBasicProfile.tsx
+++ src/features/chat/components/ChattingBasicProfile.tsx
@@ -1,34 +1,48 @@
 import Image from 'next/image'
+import { ChatBasicProfileProps } from '../types/ChatTypes'
 
-export default function ChattingBasicProfile() {
+export default function ChattingBasicProfile({
+  chatPartnerName,
+  chatPartnerImageUrl,
+  majorPosition,
+  cityName,
+  divisionName,
+  chatPartnerOnline,
+}: ChatBasicProfileProps) {
   return (
-    <>
-      {/* 기본 정보 */}
-      <div className="flex w-full gap-4 rounded-xl bg-white px-[1.88rem] py-3">
-        <Image src={'/common/default_profile.svg'} width={70} height={70} alt="Profile" />
-
-        <div className="flex w-full items-center justify-between">
-          <div className="flex flex-col">
-            <div className="flex gap-3">
-              <span className="text-lg font-semibold text-grey90">김링킷</span>
-
+    <div className="flex w-full gap-4 rounded-xl bg-white px-[1.88rem] py-3">
+      <div className="relative h-[70px] w-[80px] rounded-lg">
+        <Image
+          src={chatPartnerImageUrl || '/common/default_profile.svg'}
+          alt="Profile"
+          fill
+          className="rounded-lg object-cover"
+        />
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col">
+          <div className="flex gap-3">
+            <span className="text-lg font-semibold text-grey90">{chatPartnerName}</span>
+            {chatPartnerOnline && (
               <span className="flex items-center gap-1">
                 <Image src={'/common/icons/blue_circle.svg'} width={6} height={6} alt="circle" />
                 <span className="text-xs text-[#4D82F3]">온라인</span>
               </span>
-            </div>
-
-            <div className="flex gap-2 text-xs text-grey60">
-              <span className="text-grey50">포지션 |</span> <span className="text-grey70">기획</span>
-            </div>
-            <div className="flex gap-2 text-xs text-grey60">
-              <span className="text-grey50">지역 |</span> <span className="text-grey70">서울시 마포구</span>
-            </div>
+            )}
           </div>
-
-          <Image src={'/common/icons/more_row.svg'} width={22} height={22} alt="more_row" className="cursor-pointer" />
+          <div className="flex gap-2 text-xs text-grey60">
+            <span className="text-grey50">포지션 |</span>
+            <span className="text-grey70">{majorPosition}</span>
+          </div>
+          <div className="flex gap-2 text-xs text-grey60">
+            <span className="text-grey50">지역 |</span>
+            <span className="text-grey70">
+              {cityName} {divisionName}
+            </span>
+          </div>
         </div>
+        <Image src={'/common/icons/more_row.svg'} width={22} height={22} alt="more_row" className="cursor-pointer" />
       </div>
-    </>
+    </div>
   )
 }
