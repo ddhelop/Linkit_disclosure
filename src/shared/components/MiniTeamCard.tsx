a15b27aa7bef6dc1824eee3a956a676066a6d3fa
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-29 17:41:27 +0900

    feat: 스크랩 상태 렌더링 업데이트, 팀 상태 2개 제한

--- src/shared/components/MiniTeamCard.tsx
+++ src/shared/components/MiniTeamCard.tsx
@@ -14,19 +14,20 @@ interface MiniTeamCardProps {
 export default function MiniTeamCard({ teamInfo }: MiniTeamCardProps) {
   const [isTeamScrap, setIsTeamScrap] = useState(teamInfo?.result?.teamInformMenu.isTeamScrap ?? false)
   const [isScrapLoading, setIsScrapLoading] = useState(false)
+  const [scrapCount, setScrapCount] = useState(teamInfo?.result?.teamInformMenu.teamScrapCount ?? 0)
   const toast = useToast()
 
   const handleScrapClick = async (e: React.MouseEvent) => {
     e.preventDefault()
     if (isScrapLoading) return
+
     try {
       setIsScrapLoading(true)
-      const nextScrapState = !isTeamScrap
-      const response = await teamScrap(teamInfo?.result?.teamInformMenu.teamCode || '', nextScrapState)
-      if (response.ok) {
-        setIsTeamScrap(nextScrapState)
-        toast.success('스크랩 상태가 변경되었습니다.')
-      }
+
+      await teamScrap(teamInfo?.result?.teamInformMenu.teamCode || '', !isTeamScrap)
+      setIsTeamScrap(!isTeamScrap)
+      setScrapCount((prev) => (isTeamScrap ? prev - 1 : prev + 1))
+      toast.success('스크랩 상태가 변경되었습니다.')
     } catch (error) {
       toast.alert('스크랩 상태 변경에 실패했습니다.')
     } finally {
@@ -42,11 +43,16 @@ export default function MiniTeamCard({ teamInfo }: MiniTeamCardProps) {
     >
       <div className="flex justify-between">
         <div className="flex gap-2">
-          {teamInfo?.result.teamInformMenu.teamCurrentStates.map((state, index) => (
+          {teamInfo?.result.teamInformMenu.teamCurrentStates.slice(0, 2).map((state, index) => (
             <div key={index} className="rounded-[0.38rem] bg-[#EDF3FF] px-2 py-1 text-xs text-[#3774F4]">
               {state.teamStateName}
             </div>
           ))}
+          {teamInfo?.result.teamInformMenu.teamCurrentStates.length > 2 && (
+            <div className="rounded-[0.38rem] bg-[#EDF3FF] px-2 py-1 text-xs text-[#3774F4]">
+              +{teamInfo.result.teamInformMenu.teamCurrentStates.length - 2}
+            </div>
+          )}
         </div>
         <button onClick={handleScrapClick} className="relative h-5 w-5">
           <Image
@@ -69,6 +75,7 @@ export default function MiniTeamCard({ teamInfo }: MiniTeamCardProps) {
         <div className="flex flex-col justify-center">
           <div className="flex items-center gap-3">
             <span className="text-lg font-semibold text-grey90">{teamInfo?.result.teamInformMenu.teamName}</span>
+            <span className="text-xs text-grey70">스크랩 수 {scrapCount}</span>
           </div>
 
           <div className="mt-1 flex flex-col ">
