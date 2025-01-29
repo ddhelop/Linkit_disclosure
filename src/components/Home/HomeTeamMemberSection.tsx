2820893a673b1df0777c3c2777143f968839e311
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-01-29 19:50:07 +0900

    feat: 홈화면 섹션별 전체보기 링크 연결

--- src/components/Home/HomeTeamMemberSection.tsx
+++ src/components/Home/HomeTeamMemberSection.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import { getTeamMemberRecommend } from './api/HomeApi'
 import { ProfileInform } from '@/features/match/types/MatchTypes'
 import MiniProfileCard_2 from '@/shared/components/MiniProfileCard_2'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function HomeTeamMemberSection() {
   const [teamMemberRecommend, setTeamMemberRecommend] = useState<ProfileInform[]>([])
@@ -16,7 +18,13 @@ export default function HomeTeamMemberSection() {
   }, [])
   return (
     <div className="flex w-full flex-col gap-6">
-      <h1 className="text-xl font-semibold">이런 팀원은 어때요?</h1>
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-xl font-semibold">이런 팀원은 어때요?</h1>
+        <Link href="/find/private" className="flex cursor-pointer items-center gap-1">
+          <span className="text-sm text-grey60 ">전체보기</span>
+          <Image src="/common/icons/arrow-right(greyblack).svg" alt="arrow_right" width={16} height={16} />
+        </Link>
+      </div>
 
       <div className="grid grid-cols-3 gap-6">
         {teamMemberRecommend.map((profile) => (
--- src/components/Home/HomeTeamSection.tsx
+++ src/components/Home/HomeTeamSection.tsx
@@ -4,6 +4,8 @@ import MiniTeamCard_2 from '@/shared/components/MiniTeamCard_2'
 import { useEffect, useState } from 'react'
 import { getTeamRecommend } from './api/HomeApi'
 import { Team } from '@/features/find/types/FindTypes'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function HomeTeamSection() {
   const [teamRecommend, setTeamRecommend] = useState<Team[]>([])
@@ -18,8 +20,13 @@ export default function HomeTeamSection() {
 
   return (
     <div className="flex w-full flex-col gap-6">
-      <h1 className="text-xl font-semibold">팀원으로 합류해 보세요</h1>
-
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-xl font-semibold">팀원으로 합류해 보세요</h1>
+        <Link href="/find/team" className="flex cursor-pointer items-center gap-1">
+          <span className="text-sm text-grey60 ">전체보기</span>
+          <Image src="/common/icons/arrow-right(greyblack).svg" alt="arrow_right" width={16} height={16} />
+        </Link>
+      </div>
       <div className="grid grid-cols-2 gap-6">
         {teamRecommend.map((team) => (
           <MiniTeamCard_2 key={team.teamCode} team={team} />
