78ee46debfb80e2d98294dd9b8f7536880668811
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-02 15:14:32 +0900

    style: 팀 로그 카드 스타일 변경

--- src/components/Home/HomeLogSection.tsx
+++ src/components/Home/HomeLogSection.tsx
@@ -6,7 +6,7 @@ export default function HomeLogSection({ popularLog }: { popularLog: ILogCard[] 
     <div className="flex w-full flex-col gap-6">
       <h1 className="text-xl font-semibold">오늘의 인기 로그</h1>
 
-      <div className="grid grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 gap-6">
         {popularLog.map((log) => (
           <LogCard key={log.id} log={log} />
         ))}
--- src/shared/components/Card/LogCard.tsx
+++ src/shared/components/Card/LogCard.tsx
@@ -12,45 +12,36 @@ export default function LogCard({ log }: { log: ILogCard }) {
           ? `/${log.logInformDetails.emailId}/logs/${log.id}`
           : `/team/${log.logInformDetails.teamCode}/log/${log.id}`
       }
-      className="flex h-[10rem] flex-col gap-3 rounded-xl border border-transparent px-8 py-6 hover:border-main"
+      className="flex flex-col gap-3 rounded-xl border border-transparent px-8 py-6 hover:border-main"
       style={{ boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)' }}
     >
+      <div className="flex items-start gap-2">
+        <div className="relative h-[28px] w-[28px] rounded-lg">
+          {log.domainType === 'PROFILE' ? (
+            <Image src={log.logInformDetails.profileImagePath} alt="팀 로고" fill className="rounded-lg object-cover" />
+          ) : (
+            <Image
+              src={log.logInformDetails.teamLogoImagePath}
+              alt="팀 로고"
+              fill
+              className="rounded-lg object-cover"
+            />
+          )}
+        </div>
+        {log.domainType === 'PROFILE' ? (
+          <span className="text-sm font-semibold text-grey90">{log.logInformDetails.memberName}</span>
+        ) : (
+          <span className="text-sm font-semibold text-grey90">{log.logInformDetails.teamName}</span>
+        )}
+      </div>
       <div className="flex justify-between">
         <div className="flex items-start gap-1">
-          <span className="w-[70%] text-lg font-semibold text-grey80">{log.logTitle}</span>
-          <div className="flex items-center gap-1">
-            <span className="px-1 text-grey50">|</span>
-            <span className="text-xs font-normal text-grey60">{new Date(log.createdAt).toLocaleDateString()}</span>
-          </div>
-        </div>
-        <div className="flex items-start gap-2">
-          <div className="relative h-[28px] w-[28px] rounded-lg">
-            {log.domainType === 'PROFILE' ? (
-              <Image
-                src={log.logInformDetails.profileImagePath}
-                alt="팀 로고"
-                fill
-                className="rounded-lg object-cover"
-              />
-            ) : (
-              <Image
-                src={log.logInformDetails.teamLogoImagePath}
-                alt="팀 로고"
-                fill
-                className="rounded-lg object-cover"
-              />
-            )}
-          </div>
-          {log.domainType === 'PROFILE' ? (
-            <span className="text-sm font-semibold text-grey90">{log.logInformDetails.memberName}</span>
-          ) : (
-            <span className="text-sm font-semibold text-grey90">{log.logInformDetails.teamName}</span>
-          )}
+          <span className="w-full text-lg font-semibold text-grey80">{log.logTitle}</span>
         </div>
       </div>
 
       {/* 내용 */}
-      <div className="text-sm text-grey60">{truncateText(stripHtmlAndImages(log.logContent), 100)}</div>
+      <div className="text-sm text-grey60">{truncateText(stripHtmlAndImages(log.logContent), 250)}</div>
     </Link>
   )
 }
