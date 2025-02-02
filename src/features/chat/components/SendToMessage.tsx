7fd682a88b3a0ee09939dc65d4d72caba38fd8fb
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-02 15:23:38 +0900

    style: 채팅방 sender Image 표시

--- src/features/chat/components/SendToMessage.tsx
+++ src/features/chat/components/SendToMessage.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { ChatMessage } from '../types/ChatTypes'
 
 interface SendToMessageProps {
@@ -6,13 +7,24 @@ interface SendToMessageProps {
 
 export default function SendToMessage({ message }: SendToMessageProps) {
   return (
-    <div className="mb-4 flex w-full justify-end">
+    <div className="mb-4 flex w-full justify-end gap-3">
       <div className="flex items-end justify-end gap-1">
         <span className="mt-1 text-xs text-grey60">
           {new Date(message.timestamp).toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit' })}
         </span>
         <div className="max-w-[87%] rounded-2xl bg-grey30 p-5">
           <p className="text-sm text-grey80">{message.content}</p>
+        </div>
+      </div>
+
+      <div className="flex items-start">
+        <div className="relative h-[48px] w-[48px] rounded-lg">
+          <Image
+            src={message.messageSenderLogoImagePath}
+            alt="프로필 이미지"
+            fill
+            className="rounded-lg object-cover"
+          />
         </div>
       </div>
     </div>
