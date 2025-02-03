c146bed5e904098c62dba89a747d65cd9e776386
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-03 13:15:29 +0900

    feat: 내 포트폴리오 리스트 뷰어

---
+++ src/app/[emailId]/portfolio/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { ProfilePortfolioItem } from '@/entities/profile/model/types'
+import { getProfilePortfolioList } from '@/entities/profile/api/profileApi'
+import ProjectComponent from '@/features/profile/edit/components/common/ProjectComponent'
+
+export default function PortfolioPage({ params }: { params: { emailId: string } }) {
+  const [profileData, setProfileData] = useState<ProfilePortfolioItem[] | null>(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      const emailId = params.emailId as string
+      const data = await getProfilePortfolioList(emailId)
+      setProfileData(data.result.profilePortfolioItems)
+    }
+    getData()
+  }, [params.emailId])
+
+  return (
+    <div className="flex flex-col px-[4.25rem] py-[3.75rem]">
+      <Link href={`/${params.emailId}`} className="flex items-center gap-2 text-xl font-semibold">
+        <Image src="/common/icons/arrow-left.svg" alt="arrow_left" width={24} height={24} />
+        뒤로가기
+      </Link>
+
+      <div className="mt-6 flex flex-col gap-6">
+        {profileData?.map((item) => (
+          <ProjectComponent
+            key={item.profilePortfolioId}
+            projectName={item.projectName}
+            projectSize={item.projectSize}
+            projectLineDescription={item.projectLineDescription}
+            projectStartDate={item.projectStartDate}
+            projectEndDate={item.projectEndDate}
+            projectRoles={item.projectRoles}
+            projectRepresentImagePath={item.projectRepresentImagePath}
+            profilePortfolioId={item.profilePortfolioId}
+            isProjectInProgress={item.isProjectInProgress}
+            isEdit={false}
+            emailId={params.emailId}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
