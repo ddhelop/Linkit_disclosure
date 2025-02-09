5e37a0a654956b7625443414a143d46c485c870c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-09 15:25:59 +0900

    style: 헤더 각 섹션별 url에 따른 스타일 변경

--- src/widgets/Header/components/Navigation.tsx
+++ src/widgets/Header/components/Navigation.tsx
@@ -1,25 +1,34 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Navigation() {
+  const pathname = usePathname()
+
   return (
     <div className="ml-12 hidden h-full items-center text-grey60 md:flex">
       <Link
         href="/find/private"
-        className="mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 border-transparent pb-2 hover:border-main hover:text-grey100"
+        className={`mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 pb-2 hover:border-main hover:text-grey100 ${
+          pathname === '/find/private' ? 'border-main text-grey100' : 'border-transparent'
+        }`}
       >
         팀원
       </Link>
       <Link
         href="/find/team"
-        className="mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 border-transparent pb-2 hover:border-main hover:text-grey100"
+        className={`mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 pb-2 hover:border-main hover:text-grey100 ${
+          pathname === '/find/team' ? 'border-main text-grey100' : 'border-transparent'
+        }`}
       >
         팀
       </Link>
       <Link
         href="/find/announcement"
-        className="mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 border-transparent pb-2 hover:border-main hover:text-grey100"
+        className={`mt-2 flex h-full w-[6.12rem] items-center justify-center border-b-2 pb-2 hover:border-main hover:text-grey100 ${
+          pathname === '/find/announcement' ? 'border-main text-grey100' : 'border-transparent'
+        }`}
       >
         모집 공고
       </Link>
