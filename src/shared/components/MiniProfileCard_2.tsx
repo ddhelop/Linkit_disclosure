764696f99de734a6553ab4234179d352c58b62a4
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-13 19:29:21 +0900

    feat: 팀원 찾기 모바일 반응형

--- src/shared/components/MiniProfileCard_2.tsx
+++ src/shared/components/MiniProfileCard_2.tsx
@@ -47,7 +47,7 @@ export default function MiniProfileCard_2({ profile }: MiniProfileCard2Props) {
   return (
     <Link
       href={`/${profile.emailId}`}
-      className="flex min-w-[24rem] cursor-pointer flex-col rounded-xl border border-transparent bg-[#EDF3FF] p-[1.37rem] pb-7 hover:border-main md:min-w-[unset]"
+      className="2xl:min-w-[24rem] flex cursor-pointer flex-col rounded-xl border border-transparent bg-[#EDF3FF] p-[1.37rem] pb-7 hover:border-main "
       style={{ boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)' }}
     >
       {/* 첫째줄 */}
