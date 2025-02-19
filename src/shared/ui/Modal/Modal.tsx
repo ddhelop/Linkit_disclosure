60b940f1626f2c95af4516b494130f5a7a73b7ff
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-19 15:02:32 +0900

    feat: 공용 모달 컴포넌트에 Close 버튼 추가

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useRef } from 'react'
 import { useOnClickOutside } from '@/shared/hooks/useOnClickOutside'
+import Image from 'next/image'
 
 export default function Modal({
   isOpen,
@@ -30,8 +31,11 @@ export default function Modal({
   if (!isOpen) return null
 
   return (
-    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black bg-opacity-50">
-      <div ref={modalRef} className="relative rounded-xl bg-white">
+    <div className="fixed inset-0 z-[110] flex items-center justify-center bg-black bg-opacity-50">
+      <div ref={modalRef} className="relative h-full w-full bg-white md:h-auto md:w-auto md:rounded-xl">
+        <button onClick={onClose} className="absolute right-6 top-6 cursor-pointer" aria-label="Close modal">
+          <Image src="/common/icons/delete_icon.svg" alt="close" width={24} height={24} />
+        </button>
         {children}
       </div>
     </div>
