8803a322fe978967f1a6373dba45406b9333f369
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-20 14:34:16 +0900

    chore: 팀 카드 그림자 짤림 버그 수정

--- src/shared/components/MiniTeamCard_2.tsx
+++ src/shared/components/MiniTeamCard_2.tsx
@@ -49,8 +49,11 @@ export default function MiniTeamCard_2({ team }: MiniTeamCard_2Props) {
   return (
     <Link
       href={`/team/${team.teamCode}/log`}
-      className="flex min-w-[16rem] cursor-pointer flex-col rounded-xl border border-transparent px-7 py-[1.12rem] hover:border-[#7EA5F8] md:min-w-[unset]"
-      style={{ boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)' }}
+      className="flex min-w-[16rem] cursor-pointer flex-col rounded-xl border border-transparent bg-white p-[1.12rem] px-7 hover:border-[#7EA5F8] md:min-w-[unset]"
+      style={{
+        boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)',
+        margin: '1px', // 그림자가 잘리지 않도록 최소 마진 추가
+      }}
     >
       <div className="flex justify-between">
         <div className="flex gap-2">
