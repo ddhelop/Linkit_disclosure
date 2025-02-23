bd49697162d13add4a41d19e0734e62414df04be
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-02-24 01:49:35 +0900

    feat: chat, notification 버튼 컴포넌트 활용 (모바일 배치를 위함)

---
+++ src/widgets/Header/components/IconButtons/NotificationButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+import NotificationMenu from '../NotificationMenu'
+import useNotificationStore from '@/shared/store/useNotificationStore'
+
+interface NotificationButtonProps {
+  emailId: string
+}
+
+export default function NotificationButton({ emailId }: NotificationButtonProps) {
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false)
+  const unreadNotificationCount = useNotificationStore((state) => state.unreadNotificationCount)
+
+  const toggleNotification = () => {
+    setIsNotificationOpen(!isNotificationOpen)
+  }
+
+  return (
+    <div className="relative">
+      <div className="flex cursor-pointer items-center" onClick={toggleNotification}>
+        <Image src={'/common/icons/alarm_circle.svg'} width={32} height={32} alt="notification" />
+        {unreadNotificationCount > 0 && <div className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500" />}
+      </div>
+      <NotificationMenu isOpen={isNotificationOpen} onClose={() => setIsNotificationOpen(false)} emailId={emailId} />
+    </div>
+  )
+}
