8a222fd286bc59f249582edbd2131977556e1e95
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-03-15 14:41:19 +0900

    style: TeamCreate component 반응형 적용

--- src/app/team/new/page.tsx
+++ src/app/team/new/page.tsx
@@ -3,8 +3,8 @@ import TeamCreate from '@/features/team/TeamCreate'
 export default function TeamNewPage() {
   return (
     <div className="flex w-full justify-center py-[3.62rem]">
-      <div className="flex w-[55%] flex-col gap-5">
-        <h1 className="text-xl font-semibold">팀 생성을 위해 기본 정보를 입력해 주세요</h1>
+      <div className="flex flex-col gap-5 lg:w-[55%]">
+        <h1 className="px-4 font-semibold md:text-xl">팀 생성을 위해 기본 정보를 입력해 주세요</h1>
         <TeamCreate />
       </div>
     </div>
