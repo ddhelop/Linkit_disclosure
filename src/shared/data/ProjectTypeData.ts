7e7dd868a916d0e2a63bee17bf2da364431c94d1
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-03-21 16:25:19 +0900

    feat: 팀 모집공고 생성, project type, work type selection 추가

---
+++ src/shared/data/ProjectTypeData.ts
@@ -0,0 +1,14 @@
+export const projectTypeData = [
+  {
+    label: '창업·스타트업',
+    value: '창업·스타트업',
+  },
+  {
+    label: '사이드 프로젝트',
+    value: '사이드 프로젝트',
+  },
+  {
+    label: '스터디',
+    value: '스터디',
+  },
+]
---
+++ src/shared/data/WorkTypeData.ts
@@ -0,0 +1,14 @@
+export const workTypeData = [
+  {
+    label: '대면',
+    value: '대면',
+  },
+  {
+    label: '비대면',
+    value: '비대면',
+  },
+  {
+    label: '대면·비대면 혼합',
+    value: '대면·비대면 혼합',
+  },
+]
