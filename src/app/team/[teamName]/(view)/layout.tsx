4a5de109c5ccdc85f11b8347fe15bfbdef27d033
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-03-22 22:55:45 +0900

    feat: implement dynamic metadata generation for team pages

--- src/app/team/[teamName]/(view)/layout.tsx
+++ src/app/team/[teamName]/(view)/layout.tsx
@@ -1,6 +1,30 @@
+import { getTeamDetail } from '@/features/team-view/api/TeamDataViewApi'
 import { loadTeamData } from '@/features/team-view/loader'
 import TeamViewClient from '@/features/team-view/ui/teamInfo/TeamViewClient'
 import { HydrationBoundary } from '@tanstack/react-query'
+
+export async function generateMetadata({ params }: { params: { teamName: string } }) {
+  const teamName = params.teamName
+  const teamData = await getTeamDetail(teamName)
+  const name = teamData.result.teamInformMenu.teamName
+  const teamLogoImage = teamData.result.teamInformMenu.teamLogoImagePath
+  return {
+    title: `${name}`,
+    description: `${name} 팀 소개`,
+    openGraph: {
+      title: `${name}`,
+      description: '팀 소개 페이지입니다.',
+      url: `https://linkit.im/team/${teamName}/log`,
+      images: [
+        {
+          url: teamLogoImage,
+          width: 600,
+          height: 600,
+        },
+      ],
+    },
+  }
+}
 
 export default async function TeamLayout({
   children,
