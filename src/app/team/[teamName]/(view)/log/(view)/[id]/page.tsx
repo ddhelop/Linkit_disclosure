277727b0b627ae27d171d55a22e9e82960ef9f67
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-04-02 16:58:56 +0900

    feat: 팀 로그 레이아웃 재정의 및 TeamViewWideInfo 컴포넌트 적용

--- src/app/team/[teamName]/(view)/log/(view)/[id]/page.tsx
+++ src/app/team/[teamName]/(view)/log/(view)/[id]/page.tsx
@@ -1,35 +1,29 @@
 import { getTeamDetail } from '@/features/team-view/api/TeamDataViewApi'
+import TeamViewWideInfo from '@/features/team-view/ui/TeamViewWideInfo'
 import TeamViewDetail from '@/features/team/view/log/TeamViewDetail'
-import MiniTeamCard from '@/shared/components/MiniTeamCard'
 import { Button } from '@/shared/ui/Button/Button'
 import Link from 'next/link'
 
 export default async function TeamViewLogDetailPage({ params }: { params: { id: number; teamName: string } }) {
   const { teamName, id } = params
   const teamInfo = await getTeamDetail(teamName)
+
   return (
     <>
-      <div className="flex w-full justify-center px-4 pb-10">
-        <div className="flex w-full flex-col items-center gap-8  pt-5 lg:w-[83%] lg:py-[3.62rem]">
-          <div className="flex w-full flex-col gap-8 lg:flex-row">
-            <TeamViewDetail teamName={teamName} id={id} />
-            <div className="flex justify-center">
-              <div>
-                <MiniTeamCard teamInfo={teamInfo.result.teamInformMenu} />
-              </div>
-            </div>
-          </div>
-
-          <Link href={`/team/${teamName}/log`} className="flex w-full ">
-            <Button
-              className=" rounded-lg border border-grey40 bg-[#fcfcfd] text-grey70"
-              animationMode="grey"
-              mode="custom"
-            >
-              목록으로
-            </Button>
-          </Link>
+      <div className="flex w-full flex-col items-center gap-10 pb-10">
+        <div className="flex w-full flex-col gap-8 lg:w-[90%] lg:flex-row">
+          <TeamViewDetail teamName={teamName} id={id} />
         </div>
+
+        <Link href={`/team/${teamName}/log`} className="flex w-full lg:w-[90%]">
+          <Button
+            className=" rounded-lg border border-grey40 bg-[#fcfcfd] text-grey70"
+            animationMode="grey"
+            mode="custom"
+          >
+            목록으로
+          </Button>
+        </Link>
       </div>
     </>
   )
