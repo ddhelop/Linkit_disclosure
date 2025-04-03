08ad8e4852565bd9391aa3a43fba045bab6da5a7
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-04-03 16:42:41 +0900

    feat: 개인/팀 로그 공유하기(링크 복사) 기능 추가

--- src/features/profile-view/ui/ProfileViewLogDetail.tsx
+++ src/features/profile-view/ui/ProfileViewLogDetail.tsx
@@ -2,7 +2,9 @@
 import { getProfileLogDetail } from '@/features/profile/api/profileLogApi'
 import useLinkifyContent from '@/shared/hooks/useLinkifyContent'
 import { useScrollTopOnMount } from '@/shared/hooks/useScrollTopOnMount'
+import ShareLinkButton from '@/shared/components/ShareLinkButton'
 import { useQuery } from '@tanstack/react-query'
+import Image from 'next/image'
 
 export default function ProfileViewLogDetail({ profileLogId }: { profileLogId: number }) {
   useScrollTopOnMount()
@@ -18,11 +20,15 @@ export default function ProfileViewLogDetail({ profileLogId }: { profileLogId: n
   return (
     <>
       <div className="flex w-full flex-col gap-4 border-grey40 bg-white p-4 lg:rounded-xl lg:border lg:border-grey40 lg:p-6  ">
-        <div className="flex items-center gap-2">
-          {profileLog?.logType === 'REPRESENTATIVE_LOG' && (
-            <div className="rounded-md bg-grey20 p-2 text-xs font-normal text-main">대표글</div>
-          )}
-          <span className="font-semibold text-grey80">{profileLog?.logTitle}</span>
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            {profileLog?.logType === 'REPRESENTATIVE_LOG' && (
+              <div className="rounded-md bg-grey20 p-2 text-xs font-normal text-main">대표글</div>
+            )}
+            <span className="font-semibold text-grey80">{profileLog?.logTitle}</span>
+          </div>
+
+          <ShareLinkButton />
         </div>
         <div className="flex items-center gap-2">
           <span className="text-xs font-normal text-grey60">
