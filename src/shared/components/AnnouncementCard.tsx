022dea9042661f09861277b44dd1bb369908bf2e
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-04-08 15:36:56 +0900

    style: 홈화면 카드들 일괄 스트로크 속성 변경

--- src/shared/components/AnnouncementCard.tsx
+++ src/shared/components/AnnouncementCard.tsx
@@ -57,7 +57,7 @@ export default function AnnouncementCard({
       className={
         variant == 'wide'
           ? 'flex min-w-[17rem] cursor-pointer flex-col gap-3 border-b border-grey40 px-2 py-6 last:border-none hover:bg-grey10 md:min-w-[unset] md:px-4'
-          : 'shadow-announcement flex min-w-[17rem] cursor-pointer flex-col gap-3 rounded-lg border bg-grey10 px-[1.62rem] py-[1.38rem] hover:border-[#7EA5F8] md:min-w-[unset]'
+          : 'flex min-w-[17rem] cursor-pointer flex-col gap-3 rounded-lg border border-grey30 bg-[#FCFCFD] px-[1.62rem] py-[1.38rem] shadow-announcement hover:border-[#4D82F3] md:min-w-[unset]'
       }
     >
       <div className="flex justify-between">
@@ -72,14 +72,16 @@ export default function AnnouncementCard({
               ? '마감'
               : `D-${announcement?.announcementDDay}`}
         </span>
-        <Image
-          src={announcement.isAnnouncementScrap ? '/common/icons/save.svg' : '/common/icons/not_save.svg'}
-          alt="announcement-icon"
-          width={20}
-          height={20}
-          onClick={handleScrap}
-          className="cursor-pointer"
-        />
+        {variant === 'wide' && (
+          <Image
+            src={announcement.isAnnouncementScrap ? '/common/icons/save.svg' : '/common/icons/not_save.svg'}
+            alt="announcement-icon"
+            width={20}
+            height={20}
+            onClick={handleScrap}
+            className="cursor-pointer"
+          />
+        )}
       </div>
 
       <div className="flex items-center gap-2">
@@ -111,9 +113,11 @@ export default function AnnouncementCard({
         </div>
       </div>
 
-      <div>
-        <span className="text-xs text-grey70">조회수 {announcement.viewCount}</span>
-      </div>
+      {variant === 'wide' && (
+        <div>
+          <span className="text-xs text-grey70">조회수 {announcement.viewCount}</span>
+        </div>
+      )}
     </Link>
   )
 }
--- src/shared/components/MiniTeamCard_2.tsx
+++ src/shared/components/MiniTeamCard_2.tsx
@@ -49,7 +49,7 @@ export default function MiniTeamCard_2({ team }: MiniTeamCard_2Props) {
   return (
     <Link
       href={`/team/${team?.teamCode}/log`}
-      className="flex min-w-[16rem] cursor-pointer flex-col rounded-xl border border-transparent bg-white p-[1.12rem] hover:border-[#7EA5F8] md:min-w-[unset]"
+      className="flex min-w-[16rem] cursor-pointer flex-col rounded-xl border border-grey30 bg-white p-[1.12rem] hover:border-[#4D82F3] md:min-w-[unset]"
       style={{
         boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.10)',
         margin: '1px', // 그림자가 잘리지 않도록 최소 마진 추가
@@ -111,6 +111,7 @@ export default function MiniTeamCard_2({ team }: MiniTeamCard_2Props) {
           </span>
         </div>
       </div>
+      <span className="mt-3 px-2 text-xs text-grey90">{team?.teamShortDescription}</span>
     </Link>
   )
 }
